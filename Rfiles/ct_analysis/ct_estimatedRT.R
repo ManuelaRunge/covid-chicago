bb2fad0b0ebac08a6d52f24aafa377e9bb1f4fdf
ManuelaRunge <manuela.runge@northwestern.edu>
2020-06-27 01:04:30 -0500

    added Rt plot per EMS

--- Rfiles/ct_analysis/ct_estimatedRT.R
+++ Rfiles/ct_analysis/ct_estimatedRT.R
@@ -35,11 +35,43 @@ Rt_dat2 <- Rt_dat2 %>% mutate(meanRtLE1 = ifelse(Median <1 , 1, 0))
 write.csv(Rt_dat2, file=file.path(Rt_dir, paste0("EMS_combined_estimated_Rt.csv")), row.names = FALSE)
 summary(Rt_dat2$Date)
 
-ggplot(data= subset(Rt_dat2, region==1)) +
+Rt_dat2$region_label <- factor(Rt_dat2$region, levels = c(1:11), labels = paste0("EMS_", c(1:11), "\n"))
+
+
+pplot <- ggplot(data= subset(Rt_dat2)) +
   theme_minimal() +
   geom_line(aes(x=Date, y =Mean , group=scen_num),col="darkgrey") +
   geom_smooth(aes(x=Date, y =Mean ),col="darkred") +
   scale_x_date(breaks = "1 month", labels = date_format("%b")) +
-  geom_hline(yintercept = 1)
+  geom_hline(yintercept = 1) +
+  facet_wrap(~region_label)
+
+
+ggsave(paste0(selected_outcome, "_Rt_over_time.png"),
+       plot = pplot, path = file.path(exp_dir), width = 12, height = 7, dpi = 300, device = "png"
+)
+
+
+Rt_dat3 <- Rt_dat2 %>%
+  left_join(peakTimes, by = c("region", "scen_num", "isolation_success", "detection_success", "grpvar")) %>%
+  filter(Date==Date_peak)
+
+
+pplot <- ggplot(data= subset(Rt_dat2)) +
+  theme_minimal() +
+  geom_line(aes(x=Date, y =Mean , group=scen_num),col="darkgrey") +
+  geom_smooth(aes(x=Date, y =Mean ),col="darkred") +
+  scale_x_date(breaks = "1 month", labels = date_format("%b")) +
+  geom_hline(yintercept = 1) +
+  facet_wrap(~region_label) +
+  geom_vline(data=Rt_dat3, aes(xintercept = Date_peak))
+
+ggsave(paste0(selected_outcome, "_Rt_over_time_criticalPeakDates.png"),
+       plot = pplot, path = file.path(exp_dir), width = 12, height = 7, dpi = 300, device = "png"
+)
+
+
+
+
 
 
