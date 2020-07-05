ac072ef336c80ecb58242ddd9a4e607cbe658ed3
ManuelaRunge <manuela.runge@northwestern.edu>
2020-07-04 22:05:49 -0500

    r styler heatmap script

--- Rfiles/ct_analysis/heatmap_loess_contactTracing.R
+++ Rfiles/ct_analysis/heatmap_loess_contactTracing.R
@@ -1,7 +1,7 @@
 ## --------------------------------------------
 ## Generate heatmap and supplementary plots per EMS or aggregated region
+### Using loess regression per EMS and grpvar
 ## --------------------------------------------
-require(reshape)
 
 
 if (geography == "EMS") emsregions <- c(1:11)
@@ -19,80 +19,70 @@ for (ems in emsregions) {
   capacity <- load_capacity(selected_ems)
   capacity$deaths <- 0
 
-
   tempdat <- getdata(trajectoriesDat, selected_ems)
   tempdat$region <- ems
-  # write.csv(tempdat, file.path(ems_dir, paste0(geography, "_", ems, "_dat.csv")))
 
-  # outcomeList <- c("critical", "hospitalized", "deaths", "ventilators")
-  # Focus on ICU beds
-  outcomeList <- c("critical")
+  capacityline <- as.numeric(capacity[colnames(capacity) == selected_outcome])
 
-  for (selected_outcome in outcomeList) {
-    # selected_outcome = "critical"
+  ## Genrate outcome variable (renamed from selected outcome)
+  if (selected_outcome == "ventilators") {
+    plotdat <- tempdat %>%
+      filter(outcome == "crit_det") %>%
+      mutate(
+        value = value * 0.660,
+        outcome = selected_outcome
+      ) %>%
+      mutate(region = ems) %>%
+      as.data.frame()
+  } else {
+    plotdat <- tempdat %>%
+      filter(outcome == selected_outcome) %>%
+      mutate(region = ems) %>%
+      as.data.frame()
+  }
 
-    capacityline <- as.numeric(capacity[colnames(capacity) == selected_outcome])
+  ### Identify peak at least 10 days after reopening
+  peakTimes <- plotdat %>%
+    filter(Date > min(Date) + 10) %>%
+    group_by(N, Ki, region, isolation_success, detection_success, grpvar, scen_num, sample_num, run_num) %>%
+    filter(value == max(value)) %>%
+    dplyr::rename("Date_peak" = Date) %>%
+    select(N, Ki, region, Date_peak, outcome, isolation_success, detection_success, grpvar, scen_num, sample_num, run_num)
 
-    if (selected_outcome == "ventilators") {
-      plotdat <- tempdat %>%
-        filter(outcome == "crit_det") %>%
-        mutate(
-          value = value * 0.660,
-          outcome = selected_outcome
-        ) %>%
-        mutate(region = ems) %>%
-        as.data.frame()
-    } else {
-      plotdat <- tempdat %>%
-        filter(outcome == selected_outcome) %>%
-        mutate(region = ems) %>%
-        as.data.frame()
-    }
+  summary(peakTimes$Date_peak)
 
+  ## Add peak date to plotdat
+  plotdat <- plotdat %>%
+    left_join(peakTimes, by = c("N", "Ki", "region", "outcome", "scen_num", "sample_num", "run_num", "isolation_success", "detection_success", "grpvar"))
 
-    peakTimes <- plotdat %>%
-      filter(Date > min(Date) + 10) %>%
-      group_by(N, Ki, region, isolation_success, detection_success, grpvar, scen_num, sample_num, run_num) %>%
-      filter(value == max(value)) %>%
-      dplyr::rename("Date_peak" = Date) %>%
-      select(N, Ki, region, Date_peak, outcome, isolation_success, detection_success, grpvar, scen_num, sample_num, run_num)
-    summary(peakTimes$Date_peak)
+  ### Scatter plot  of outcome variable per CT parameter at peak
+  showScatter <- TRUE
+  if (showScatter) {
+    scatterplot <- plotdat %>%
+      filter(Date == Date_peak) %>%
+      mutate(
+        capacity = capacityline,
+        belowCapacity = ifelse(value <= capacity, "yes", "no")
+      ) %>%
+      ggplot() +
+      theme_minimal() +
+      geom_point(aes(x = detection_success, y = isolation_success, fill = value, group = scen_num), size = 3, shape = 21) +
+      # geom_jitter(aes(x = detection_success, y = isolation_success, fill = value, group = scen_num), size = 3, shape = 21, width = 0.025, height = 0.025) +
+      scale_fill_viridis(option = "C", discrete = FALSE, direction = -1) +
+      customThemeNoFacet
 
-    ## Add peak date to plotdat
-    plotdat <- plotdat %>%
-      left_join(peakTimes, by = c("N", "Ki", "region", "outcome", "scen_num", "sample_num", "run_num", "isolation_success", "detection_success", "grpvar"))
+    ggsave(paste0(ems, "_scatterplot.pdf"),
+      plot = scatterplot, path = file.path(ems_dir), width = 5, height = 4, device = "pdf"
+    )
 
-    showScatter <- FALSE
-    if (showScatter) {
-      scatterplot <- plotdat %>%
-        filter(Date == Date_peak) %>%
-        mutate(
-          capacity = capacityline,
-          belowCapacity = ifelse(value <= capacity, "yes", "no")
-        ) %>%
-        ggplot() +
-        theme_minimal() +
-        geom_point(aes(x = detection_success, y = isolation_success, fill = value, group = scen_num), size = 3, shape = 21) +
-        scale_fill_viridis(option = "C", discrete = FALSE, direction = -1) +
-        customThemeNoFacet
-
-      ggsave(paste0(ems, "_scatterplot.pdf"),
-        plot = scatterplot, path = file.path(ems_dir), width = 5, height = 4, device = "pdf"
-      )
-
-      # plotdat %>% filter(Date == plotdat$Date_peak) %>% write.csv(file.path(ems_dir,paste0(ems, "_scatterplot_dat.csv")), row.names = FALSE)
-    }
-
+    # plotdat %>% filter(Date == plotdat$Date_peak) %>% write.csv(file.path(ems_dir,paste0(ems, "_scatterplot_dat.csv")), row.names = FALSE)
 
     dat <- plotdat %>% filter(Date == plotdat$Date_peak)
     capacity <- capacityline
 
-
-
     fitlist <- list()
-
     for (grp in unique(dat$grpvar)) {
-      #grp  <- unique(dat$grpvar)[1]
+      # grp  <- unique(dat$grpvar)[1]
       #### Do loess regression
       detection_success <- seq(0, 1, 0.001)
       isolation_success <- seq(0, 1, 0.001)
@@ -161,17 +151,18 @@ for (ems in emsregions) {
       labels = c(">400", "<400", "<300", "<200", "<100", "<50")
     )
 
-
+    ### Extract  minimum isolation_success for each detection_success
     thresholdDat <- dtfit %>%
       filter(value <= capacity) %>%
       group_by(detection_success, grpvar) %>%
       filter(isolation_success == min(isolation_success)) %>%
       mutate(regin = ems)
 
+    ### Plot contour-heatmap plot
     p1d <- ggplot(data = subset(dtfit, !is.na(value_fct)), aes(x = detection_success, y = isolation_success)) +
       theme_minimal() +
       geom_tile(aes(fill = value_fct), alpha = 0.8) +
-      #geom_point(data=subset(dat, grpvar==0.17),  aes(x = detection_success, y = isolation_success, fill = value_fct, group = scen_num), size = 3, shape = 21, show.legend = FALSE) +
+      # geom_point(data=subset(dat, grpvar==0.17),  aes(x = detection_success, y = isolation_success, fill = value_fct, group = scen_num), size = 3, shape = 21, show.legend = FALSE) +
       geom_line(data = subset(thresholdDat, isolation_success != min(isolation_success)), aes(x = detection_success, y = isolation_success), size = 1.3) +
       scale_fill_viridis(option = "C", discrete = TRUE) +
       labs(
@@ -197,25 +188,24 @@ for (ems in emsregions) {
     )
 
     ggsave(paste0(plotname1, ".pdf"),
-      plot = p1, path = file.path(ems_dir), width = 12, height = 5,  device = "pdf"
+      plot = p1, path = file.path(ems_dir), width = 12, height = 5, device = "pdf"
     )
 
     write.csv(thresholdDat, file = file.path(ems_dir, paste0(ems, "_loess_thresholds.csv")), row.names = FALSE)
   }
-  
-}
+} 
 
 
-### Combine all EMS tresholds into one file 
+### Combine all EMS thresholds into one file
 thresholdsfiles <- list.files(file.path(ems_dir), pattern = "loess_thresholds", recursive = TRUE, full.names = TRUE)
 lmthresholdsDat <- sapply(thresholdsfiles, read.csv, simplify = FALSE) %>%
-  bind_rows(.id = "id")   
+  bind_rows(.id = "id")
 
-if(dim(lmthresholdsDat)[1]<=1)next
+if (dim(lmthresholdsDat)[1] <= 1) next
 
-lmthresholdsDat$id = gsub(ems_dir, "", lmthresholdsDat$id )
-lmthresholdsDat$region = gsub("_loess_thresholds.csv","", lmthresholdsDat$id )
-lmthresholdsDat$region = gsub("[/]","", lmthresholdsDat$region )
-lmthresholdsDat <- lmthresholdsDat[,!(colnames(lmthresholdsDat) == "regin")] 
+lmthresholdsDat$id <- gsub(ems_dir, "", lmthresholdsDat$id)
+lmthresholdsDat$region <- gsub("_loess_thresholds.csv", "", lmthresholdsDat$id)
+lmthresholdsDat$region <- gsub("[/]", "", lmthresholdsDat$region)
+lmthresholdsDat <- lmthresholdsDat[, !(colnames(lmthresholdsDat) == "regin")]
 
 write.csv(lmthresholdsDat, file.path(exp_dir, "thresholds_loess.csv"), row.names = FALSE)
