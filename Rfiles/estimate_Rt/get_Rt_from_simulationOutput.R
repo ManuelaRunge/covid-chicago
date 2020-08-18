cf2993d2701c68c3608b8cb63d538aee6af4751a
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-17 22:32:35 -0500

    updated getRt script to use outputs per scen_num

--- Rfiles/estimate_Rt/get_Rt_from_simulationOutput.R
+++ Rfiles/estimate_Rt/get_Rt_from_simulationOutput.R
@@ -32,63 +32,82 @@ outdir <- file.path("estimate_Rt/from_simulations")
 
 
 ### Load simulation outputs
-dat <- read.csv(file.path(project_path, "NU_civis_outputs",simdate,paste0("csv/nu_il_", exp_scenario ,"_",simdate,".csv")))
-summary(as.Date(dat$Date))
+#dat <- read.csv(file.path(project_path, "NU_civis_outputs",simdate,paste0("csv/nu_il_", exp_scenario ,"_",simdate,".csv")))
+Rt_dat <- read.csv(file.path(simulation_output, exp_name, 'estimatedRt', 'combined_temp_Rt_tempdat_All.csv'))
 
-method <- "uncertain_si"
-weekwindow=13
-Rt_list <- list()
-si_list <- list()
-
-for (region in unique(dat$geography_modeled)) {
-  # region = unique(dat$geography_modeled)[1]
-  disease_incidence_data <- dat %>%
-    filter(geography_modeled == region) %>%
-    rename(I = Number.of.Covid.19.new.infections)
-
-  res <- getRt(disease_incidence_data, method=method, weekwindow=weekwindow)
-
-  pplot <- plot(res)
-  ggsave(paste0(region, "_EpiEstim_default_",method,".pdf"),
-    plot = pplot, path = file.path(outdir), width = 6, height = 10, dpi = 300, device = "pdf"
-  )
-
-  Rt_list[[region]] <- res$R %>% mutate(region = region, weekwindow=weekwindow )
-  si_list[[region]] <- res$SI.Moments %>% mutate(region = region , weekwindow=weekwindow)
-}
 
 ### Combine list to dataframe 
-Rt_dat <- Rt_list %>% bind_rows() %>% 
-          mutate( time =  t_start+weekwindow ) %>%
-          dplyr::rename( geography_modeled = region)
+Rt_dat <- Rt_dat %>%  mutate( time =  t_end ) %>%
+          f_addRestoreRegion() %>%
+          select(time,scen_num, restore_region, region, Mean.R.  ,Median.R.) %>%
+          rename(meanRt =Mean.R.  ,medianRt=Median.R. )
 
+Rt_datRR <- Rt_dat %>%  group_by(restore_region, time, scen_num) %>% summarize(meanRt=mean(meanRt),medianRt=mean(medianRt) )  %>% rename(geography_modeled=restore_region)
+Rt_datIL <- Rt_dat %>%  group_by( time, scen_num) %>% summarize(meanRt=mean(meanRt),medianRt=mean(medianRt) )  %>% mutate(geography_modeled="illinois")
+
+
+Rt_dat <- Rt_dat %>% rename(geography_modeled=region) %>% select(-restore_region)%>% rbind(Rt_datRR)%>% rbind(Rt_datIL)
 table(Rt_dat$geography_modeled)
 
 
-dat <- dat %>%
-  arrange( geography_modeled, Date) %>%
-  dplyr::group_by( geography_modeled) %>%
-  mutate(date = as.Date(Date), time = c(1:n_distinct(date)))
 
-
-mergevars <- colnames(Rt_dat)[colnames(Rt_dat) %in% colnames(dat)]
 RtdatCOmbined <- Rt_dat %>%
-  merge(unique(dat), by=mergevars) %>%
-  dplyr::rename(
-    Median.of.covid.19.Rt = `Median(R)`,
-    Lower.error.bound.of.covid.19.Rt = `Quantile.0.025(R)`,
-    Upper.error.bound.of.covid.19.Rt = `Quantile.0.975(R)`
-  ) %>%
-  arrange(Date, geography_modeled) %>%
+  dplyr::group_by(time,geography_modeled ) %>%
+  dplyr::summarize(Median.of.covid.19.Rt= median(medianRt),
+            Lower.error.bound.of.covid.19.Rt=  quantile(medianRt, probs=0.025, na.rm = TRUE),
+            Upper.error.bound.of.covid.19.Rt = quantile(medianRt, probs=0.975, na.rm = TRUE)) %>%
+  dplyr::mutate(Date = as.Date('2020-02-13') + time) %>%
+dplyr::arrange(Date, geography_modeled) %>%
   filter(Date <= "2020-12-01") 
 
 
-fname =  paste0("nu_il_", exp_scenario ,"_estimated_Rt_",simdate,".csv")
-RtdatCOmbined %>% 
-  dplyr::select(Date, geography_modeled, Median.of.covid.19.Rt, Lower.error.bound.of.covid.19.Rt, Upper.error.bound.of.covid.19.Rt) %>%
-  write.csv(file.path(project_path, "NU_civis_outputs" ,simdate, 'csv',fname), row.names = FALSE)
 
+saveForCivis=FALSE
+if(saveForCivis){
+  fname =  paste0("nu_il_", exp_scenario ,"_estimated_Rt_",simdate,".csv")
+  RtdatCOmbined %>% 
+    dplyr::select(Date, geography_modeled, Median.of.covid.19.Rt, Lower.error.bound.of.covid.19.Rt, Upper.error.bound.of.covid.19.Rt) %>%
+    write.csv(file.path(project_path, "NU_civis_outputs" ,simdate, 'csv',fname), row.names = FALSE)
+  
+}
+if(saveInExpDir){
+  fname =  paste0("estimated_Rt.csv")
+  RtdatCOmbined %>% 
+    dplyr::select(Date, geography_modeled, Median.of.covid.19.Rt, Lower.error.bound.of.covid.19.Rt, Upper.error.bound.of.covid.19.Rt) %>%
+    write.csv(file.path(simulation_output, exp_name, 'estimatedRt', fname), row.names = FALSE)
 
+  
+  
+  pplot <-  RtdatCOmbined %>% 
+    filter(Date>= "2020-04-01" & Date<= "2020-09-01") %>%
+    filter(geography_modeled %in% as.character(c(1:11))) %>%
+    ggplot() + 
+    theme_minimal() +
+    geom_ribbon(aes(x=Date, ymin=Lower.error.bound.of.covid.19.Rt, ymax=Upper.error.bound.of.covid.19.Rt), fill="deepskyblue3", alpha=0.3) +
+    geom_line(aes(x=Date, y=Median.of.covid.19.Rt), col="deepskyblue3") +
+    geom_hline(yintercept = 1) +
+    facet_wrap(~geography_modeled)
+  
+  ggsave(paste0("estimatedRt_overtime.png"),
+         plot = pplot, path =file.path(simulation_output, exp_name, 'estimatedRt'), width =8, height = 6, device = "png"
+  )
+  rm(pplot)
+  
+  
+ pplot <-  RtdatCOmbined %>% 
+    filter(Date>= "2020-08-16" & Date< "2020-08-17") %>%
+    filter(geography_modeled %in% as.character(c(1:11))) %>%
+  ggplot() + 
+    theme_cowplot() +
+    geom_errorbar(aes(x=reorder(geography_modeled, Median.of.covid.19.Rt), ymin=Lower.error.bound.of.covid.19.Rt, ymax=Upper.error.bound.of.covid.19.Rt), width=0.3, alpha=0.3) +
+    geom_point(aes(x=reorder(geography_modeled, Median.of.covid.19.Rt), y=Median.of.covid.19.Rt), col="deepskyblue3",size=2.5) +
+    geom_hline(yintercept = 1)
+  
+  ggsave(paste0("estimatedRt_20200818.png"),
+         plot = pplot,path =file.path(simulation_output, exp_name, 'estimatedRt'), width = 8, height =5, device = "png"
+  )
+  
+}
 
 
 
