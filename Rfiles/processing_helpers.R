d5e75ca81cb028dfbeb7e70dd4dfe8f1f26e6314
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-18 15:15:15 -0500

    added functions to postprocessing_helper

--- Rfiles/processing_helpers.R
+++ Rfiles/processing_helpers.R
@@ -1,6 +1,79 @@
 #source("load_paths.R")
 
 
+f_valuefct = function(df){
+  
+  df$value_fct <- NA
+  df$value_fct[df$value >= 400] <- ">400"
+  df$value_fct[df$value < 400] <- "<400"
+  df$value_fct[df$value < 300] <- "<300"
+  df$value_fct[df$value < 200] <- "<200"
+  df$value_fct[df$value < 100] <- "<100"
+  df$value_fct[df$value < 50] <- "<50"
+  
+  df$value_fct <- factor(df$value_fct,
+                         levels = c(">400", "<400", "<300", "<200", "<100", "<50"),
+                         labels = c(">400", "<400", "<300", "<200", "<100", "<50")
+  )
+  
+  return(df)
+  
+}
+
+
+f_valuefct2 = function(df){
+  
+  q75 = quantile(df$value, probs=0.75, na.rm = TRUE)
+  q50 =  median(df$value, na.rm=TRUE)
+  q25 = quantile(df$value, probs=0.25, na.rm = TRUE)
+  
+  df$value_fct <- NA
+  df$value_fct[df$value >= q75] <- "> quant75"
+  df$value_fct[df$value < q75] <- "< quant75"
+  df$value_fct[df$value < q50] <- "< quant50"
+  df$value_fct[df$value < q25] <- "< quant25"
+  
+  df$value_fct <- factor(df$value_fct,
+                         levels = c("< quant25", "< quant50", "< quant75", "> quant75"),
+                         labels = c(paste0("> ", q25), 
+                                    paste0("< ", q50), 
+                                    paste0("< ", q75), 
+                                    paste0(">= ", q75))
+  )
+  
+  return(df)
+  
+}
+
+
+f_valuefct_cap = function(df, valueVar="value"){
+  
+  df= as.data.frame(df)
+  colnames(df)[colnames(df)==paste0(valueVar, "_fct")] <- paste0(valueVar, "_fct_old")
+  capacity = unique(df$capacity)
+  
+  qLT2.5 <- capacity - (capacity * 0.025)
+  qLT5 <-capacity - (capacity * 0.05)
+  qLT15 <- capacity - (capacity * 0.15)
+  
+  qGT2.5 <- capacity + (capacity * 0.025)
+  qGT5 <- capacity+ (capacity* 0.05)
+  qGT15 <-capacity + (capacity* 0.15)
+  
+  cuts <- c(-Inf,qLT15,qLT5 ,qLT2.5, capacity, qGT2.5, qGT5, qGT15, Inf)
+  
+  df$tempVar <- cut(df[,colnames(df)==valueVar] , breaks = cuts, 
+                      labels = c("- >15%","-15%","-5%","-2.5%","+2.5","+5%", "+15%", "+ >15%"))
+  
+  colnames(df)[colnames(df)=="tempVar"] <- paste0(valueVar, "_fct")
+  #tapply(df$value, df$value_fct, summary)
+
+  return(df)
+  
+}
+
+
+
 regions <- list(
   "Northcentral" = c(1, 2),
   "Northeast" = c(7, 8, 9, 10, 11),
@@ -36,6 +109,14 @@ combineDat <- function(filelist, namelist){
     if(count>=1)combinedDat <- rbind(combinedDat, dat)
   }
   return(combinedDat)
+}
+
+
+load_population <- function(){
+  geography_name <- c('illinois',c(1:11))
+  pop <- c("12830632", "688393","1269519","581432",'676017','424810','760362','791009','1432193','1012222','2477754','2716921')
+  df <-as.data.frame( cbind(geography_name, pop))
+  return(df)
 }
 
 
