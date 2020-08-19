ca0ab3ef9d8eb2ed7cb57441c8a28c19c27b77ae
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-19 13:37:11 -0500

    edits in heatmap figure draft

--- Rfiles/publication_scripts/plots_for_publication.R
+++ Rfiles/publication_scripts/plots_for_publication.R
@@ -2194,7 +2194,7 @@ if(capacityScatterPlot){
 if(heatmaps){
   
  
-f_getHeatmap_IL <- function(exp_name="20200731_IL_reopen_contactTracing", grpValues=c(0, 0.05, 0.1), showlegend=FALSE){
+f_getHeatmap_IL <- function(exp_name="20200731_IL_reopen_contactTracing",regname="illinois", grpValues=c(0.00, 0.05, 0.1), showlegend=FALSE){
   
   load(file.path(simulation_output, "contact_tracing/20200731/",exp_name,"/heatmap_ICU/Illinois_dtfit.Rdata"))
   # thresholdDat <- read_csv("C:/Users/mrm9534/Box/NU-malaria-team/projects/covid_chicago/cms_sim/simulation_output/contact_tracing/20200731/20200731_IL_reopen_contactTracing/heatmap_ICU/Illinois_loess_ICUcapacity.csv")
@@ -2203,29 +2203,38 @@ f_getHeatmap_IL <- function(exp_name="20200731_IL_reopen_contactTracing", grpVal
   
   
   
+
   dtfit <- dtfit %>%  
     filter(!is.na(value) & grpvar %in% grpValues) %>%
-    dplyr::mutate(region = 'illinois') %>%
+    dplyr::mutate(region = regname) %>%
     left_join(popdat, by="region") %>%
     left_join(capacity, by="region") %>%
-    mutate(belowCapacity = ifelse(value <= capacity, 1,0)) %>%
+    mutate(percCapacity = (value-capacity)/capacity,
+           belowCapacity = ifelse(value <= capacity, 1,0)) %>%
     mutate(value=as.numeric(value),
            pop = as.numeric(pop),
            value_pop1000 = (value /pop )*1000,
            capacity_pop1000 = (capacity /pop )*1000) %>%
-    f_valuefct_cap("value") %>%
-    f_valuefct_cap("value_pop1000")
+   f_valuefct_cap("value") 
   
-  thresholdDat <- dtfit %>%
-    dplyr::filter(value <= capacity) %>%
-    dplyr::group_by(detection_success, grpvar,capacity, pop) %>%
-    dplyr::filter(isolation_success == min(isolation_success)) 
+  labs=quantile(dtfit$percCapacity, probs = seq(0.1,1, 0.1), na.rm=FALSE)
+  
+  dtfit <- dtfit %>%    mutate(value_cut =cut(percCapacity, 10, labels=labs ) )
   
 
-  p1 <- ggplot(data = subset(dtfit,  !is.na(value_fct)), aes(x = detection_success, y = isolation_success)) +
+  thresholdDat <- dtfit %>%
+    dplyr::filter(value <= capacity ) %>%
+    dplyr::group_by(detection_success, grpvar,capacity, pop) %>%
+    dplyr::filter(isolation_success == min(isolation_success)) 
+    #dplyr::filter(percCapacity == min(percCapacity)) 
+
+  pplot <- ggplot(data = subset(dtfit,  !is.na(value_fct)), aes(x = detection_success, y = isolation_success)) +
     theme_minimal() +
-    geom_tile(aes(fill = value_fct), alpha = 0.8) +
-    scale_fill_viridis(option = "C", discrete = T) +
+    geom_tile(aes(fill = as.factor(value_cut)), alpha = 0.8) +
+    scale_fill_viridis(option = "C", discrete = T, direction=-1) +
+    geom_line(    data = subset(thresholdDat, isolation_success != min(isolation_success)),
+      aes(x = detection_success, y = isolation_success), size = 1.3
+    ) +
     labs(
       x = "detections (%)",
       y = "isolation success (%)",
@@ -2238,34 +2247,38 @@ f_getHeatmap_IL <- function(exp_name="20200731_IL_reopen_contactTracing", grpVal
     scale_y_continuous(lim = c(0, 1), breaks = seq(0, 1, 0.1), labels = seq(0, 1, 0.1) * 100, expand = c(0, 0)) +
     facet_wrap(~grpvar, nrow=1) +
     customThemeNoFacet +
-    theme(panel.spacing = unit(1.5, "lines"))
-    theme(legend.position = "right")
+    theme(panel.spacing = unit(1.5, "lines")) +
+    theme(legend.position = "right")+
+      theme(strip.text.x = element_text(size = 12, face = "bold", color="white"),
+            axis.text.y = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.title.x = element_blank(),
+            plot.margin = margin(l = 1)  )
   
-    if(showlegend==FALSE) p1 <- p1 + theme(legend.position = "none")
+    if(showlegend==FALSE) pplot <- pplot + theme(legend.position = "none")
   
-  pcounterfctual <- p1 +
-    #geom_point(
-    #data = peakdat, aes(x = detection_success, y = isolation_success, fill = value_fct),
-    #shape = 21, size = 3, show.legend = FALSE)+ 
-    geom_line(
-      data = subset(thresholdDat, isolation_success != min(isolation_success)),
-      aes(x = detection_success, y = isolation_success), size = 1.3
-    )
-  
-  return(pcounterfctual)
+  return(pplot)
 }
   
   
+
 legend <- get_legend(f_getHeatmap_IL(exp_name="20200731_IL_reopen_contactTracing", showlegend=TRUE))
 p1 <- f_getHeatmap_IL(exp_name="20200731_IL_reopen_contactTracing")
 p2 <- f_getHeatmap_IL(exp_name="20200731_IL_reopen_contactTracingHS40")
 p3 <- f_getHeatmap_IL(exp_name="20200731_IL_reopen_contactTracingHS80")
 
-ggsave(paste0("CT_scenarios_baseline", ".pdf"),
-       plot = p0, path = file.path(pdfdir), width = 12, height = 7, device = "pdf"
+
+p123 <- cowplot::plot_grid(p3 + theme(strip.text.x = element_text(size = 12, face = "bold", color="black"), 
+                   p2, 
+                   p1  ),
+                   nrow = 3,
+                   labels = "auto",
+                   align = "hv")
+
+ggsave(paste0("IL_heatmap", ".png"),
+       plot = p123, path = file.path(pdfdir), width = 8, height = 8, device = "png"
 )
 
   
 }
-
 
