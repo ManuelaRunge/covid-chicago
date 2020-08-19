0d9ebd0d06cd7703e8c9ee7069ced13cf90f6c65
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-19 16:54:45 -0500

    edited Fig detection scatterplot for critical

--- Rfiles/publication_scripts/Fig_Rt_detection_scatterplot.R
+++ Rfiles/publication_scripts/Fig_Rt_detection_scatterplot.R
@@ -83,3 +83,104 @@ ggsave(paste0("RTLE1_contactTracing_scatterPlot.pdf"),
 )
 
 
+
+
+
+##### Apply for critical 
+
+
+
+###===========================================
+###  Plot minimum detection of As and P per Rt
+###===========================================
+
+f_addRt_toICU_thresholds <- function(exp_name ="20200731_IL_reopen_contactTracing"){
+  
+  
+  dat_ct <- read.csv(file.path(ct_dir, "20200731/20200731_IL_reopen_contactTracing/CT_ICU_thresholds.csv")) %>% mutate(scenario="counterfactual")
+  dat_ctTDonly <- read.csv(file.path(ct_dir, "20200731/20200731_IL_reopen_contactTracing_TD/CT_ICU_thresholds.csv")) %>% mutate(scenario="TDonly")
+  dat_ctHS40 <- read.csv(file.path(ct_dir, "20200731/20200731_IL_reopen_contactTracingHS40/CT_ICU_thresholds.csv")) %>% mutate(scenario="HS40")
+  dat_ctHS80 <- read.csv(file.path(ct_dir, "20200731/20200731_IL_reopen_contactTracingHS80/CT_ICU_thresholds.csv")) %>% mutate(scenario="HS80")
+  dat_ctHS40TD <- read.csv(file.path(ct_dir, "20200731/20200731_IL_reopen_contactTracingHS40TD/CT_ICU_thresholds.csv")) %>% mutate(scenario="HS40TD")
+  dat_ctHS80TD <- read.csv(file.path(ct_dir, "20200731/20200731_IL_reopen_contactTracingHS80TD/CT_ICU_thresholds.csv")) %>% mutate(scenario="HS80TD")
+  
+  dat <- rbind(dat_ct, dat_ctHS40, dat_ctHS80, dat_ctHS40TD, dat_ctHS80TD)
+  popdat <- load_population() %>% rename(region =geography_name ) 
+  #popdatCentral <- popdat
+  
+  dat <- dat %>%
+    left_join(popdat, by = "region") %>%
+    group_by(region, grpvar, scenario) %>%
+    mutate(fitmax = max(isolation_success, na.rm = TRUE))
+  
+  
+  
+  
+  EMS_combined_estimated_Rt  <- read.csv(file.path(ct_dir, simdate, exp_name , "estimatedRt/EMS_combined_estimated_Rt.csv"))
+  
+  baselineRt <- EMS_combined_estimated_Rt %>% 
+    filter(Date ==as.Date("2020-03-01")) %>% 
+    dplyr::group_by(region, grpvar) %>% 
+    dplyr::summarize(baselineRt=mean(Mean)) %>%
+    mutate(region = as.character(region))
+  
+  currentRt <- EMS_combined_estimated_Rt %>% 
+    filter(Date ==as.Date("2020-07-20")) %>% 
+    dplyr::group_by(region, grpvar) %>% 
+    dplyr::summarize(currentRt=mean(Mean))%>%
+    mutate(region = as.character(region)) %>%
+    left_join(baselineRt,  by=c("region","grpvar"))
+    
+  
+  
+  ### combine data
+  datWithRt <- dat %>% left_join(currentRt, by=c("region","grpvar")) 
+  
+  customTheme <- f_getCustomTheme()
+  
+  datWithRt$scenario_fct <- factor(datWithRt$scenario,
+                                  levels = c("counterfactual", "HS40", "HS80", "TDonly", "HS40TD", "HS80TD"),
+                                  labels = c(
+                                    "current trend (comparison)",
+                                    "increase detections to 40%",
+                                    "faster testing and isolation",
+                                    "increase detections to 80%",
+                                    "increase detections to 40%\n& faster testing and isolation",
+                                    "increase detections to 80%\n& faster testing and isolation"
+                                  ))
+  
+  
+  pplot <- datWithRt %>% 
+    filter(isolation_success==fitmax & !is.na(scenario_fct) & region %in% c(1:11) ) %>% 
+    group_by(region, currentRt, scenario, scenario_fct) %>% 
+    summarize(detection_success=min(detection_success)) %>% 
+    ggplot()+ 
+    geom_point(aes(x=currentRt , y= detection_success, fill=scenario_fct),col="azure4",shape=21, size=3) +
+    geom_smooth(aes(x=currentRt , y= detection_success,col=scenario_fct,fill=scenario_fct), method="lm", alpha=0.3) +
+    scale_y_continuous(lim=c(0,1), labels = function(x) x * 100 , expand=c(0,0)) +
+    labs(x= expr(italic(R[t]) * " before contact tracing start"),
+         y="Minimum required detection of \n a - and pre-symptomatic infections (%) ",
+         color="Testing improvements for mild symptoms",
+         fill="Testing improvements for mild symptoms") +
+    scale_color_brewer(palette = "Dark2")+
+    scale_fill_brewer(palette = "Dark2") +
+    customTheme +
+    theme(legend.position = "bottom")+
+    guides(fill=guide_legend(ncol=2))+
+    guides(color=guide_legend(ncol=2))
+  
+  
+  ggsave(paste0("ICUcap_contactTracing_scatterPlot.pdf"),
+         plot = pplot, path = file.path(pdfdir), width = 9.5, height =7,  device = "pdf"
+  )
+  
+  
+  
+  
+  
+  
+  return(Rtbelow1)
+}
+
+
+
