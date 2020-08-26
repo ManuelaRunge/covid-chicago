0d5b0abbec5e12d86409e1e2a74a7dec72e9ed85
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-25 20:56:50 -0500

    cleaned rscript for trigger-slope-capacity

--- Rfiles/simulation_plotter/trigger-slope-capacity_analysis.R
+++ Rfiles/simulation_plotter/trigger-slope-capacity_analysis.R
@@ -1,24 +1,29 @@
 ## -----------------------------------------
-### Rsctipt to combine and analyse trigger analyses
+### Rscript to combine and analyze trigger analyses
 ## -----------------------------------------
 
 library(tidyverse)
 library(cowplot)
 library(scales)
 library(viridis)
+library(lubridate)
+library(broom)
+
 
 source("load_paths.R")
 source("processing_helpers.R")
 
+
+simulation_iteration <- "20200821_state_events"
+simulation_output <- file.path(simulation_output, "EMS", simulation_iteration)
+# outdir <- file.path(project_path, "Plots + Graphs/simulated_scenarios", simulation_iteration)
+outdir <- file.path(project_path, "Plots + Graphs/simulated_scenarios", "20200825_state_events")
+
 plot_first_day <- "2020-08-01"
 plot_last_day <- "2021-01-01"
 
-outdir <- file.path("C:/Users/mrm9534/Box/NU-malaria-team/projects/covid_chicago/Plots + Graphs/simulated_scenarios/20200825_state_events")
 cols <- rev(c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99"))
-
-
 theme_set(theme_cowplot())
-
 
 
 ## ------------------------------
@@ -94,82 +99,8 @@ f_loadDat <- function(exp_name) {
   return(dat)
 }
 
-
-
-## ----------------------
-### Generate plots
-## ----------------------
-exp_names_crit <- c(
-  "20200821_IL_critdet_reopen0perc_TriggeredRollback",
-  "20200820_IL_critdet_reopen5perc_TriggeredRollback",
-  "20200820_IL_critdet_reopen10perc_TriggeredRollback"
-)
-
-
-exp_names_hosp <- c(
-  "20200821_IL_hospdet_reopen0perc_TriggeredRollback",
-  "20200820_IL_hospdet_reopen5perc_TriggeredRollback",
-  "20200820_IL_hospdet_reopen10perc_TriggeredRollback"
-)
-
-
-datList <- list()
-for (exp_name in exp_names_crit) {
-
-  # exp_name <- exp_names[2]
-
-  print(exp_name)
-
-  exp_dir <- file.path(file.path(simulation_output, exp_name))
-  expLabel <- gsub("20200819_IL_", "", exp_name)
-  expLabel <- gsub("_triggeredrollback", "", expLabel)
-
-  expLabel <- gsub("20200817_IL_", "", exp_name)
-  expLabel <- gsub("_vary0to1_triggeredrollback", "", expLabel)
-
-
-  expLabel <- gsub("20200820_IL_", "", exp_name)
-  expLabel <- gsub("20200821_IL_", "", exp_name)
-  expLabel <- gsub("_reopen5perc_TriggeredRollback", "", expLabel)
-  expLabel <- gsub("_reopen10perc_TriggeredRollback", "", expLabel)
-
-  df.exp <- f_loadDat(exp_name)
-  datList[[length(datList) + 1]] <- df.exp
-
-  rm(exp_name)
-}
-
-df.exp <- datList %>% bind_rows(.id = "exp_nr")
-
-for (exp_name in exp_names_crit) {
-
-  # exp_name <- exp_names_crit[2]
-  print(exp_name)
-
-  fname <- gsub("20200820_IL_critdet_", "", exp_name)
-  fname <- gsub("20200821_IL_critdet_", "", fname)
-  fname <- gsub("_TriggeredRollback", "", fname)
-
-  reopenLabel <- paste0(gsub("perc", "", gsub("reopen", "", fname)), "% reopening scenario")
-
-  selectedEXP <- exp_name
-  subdat <- df.exp %>%
-    as.data.frame() %>%
-    filter(exp_name %in% selectedEXP)
-  table(subdat$exp_name)
-
-  summary(subdat$date)
-
-
-  tapply(subdat$timeBeforeTrigger, subdat$trigger, summary)
-
-  table(subdat$trigger, subdat$region)
-  table(subdat$trigger, subdat$changedKi)
-
-  #### Calculate weekly increase preceeding trigger
-
-  ### Visualize just slopes
-  pplot <- subdat %>%
+f_plot_prepostTrigger <- function(df, SAVE = T, savePDF = F) {
+  pplot <- df %>%
     filter((timeBeforeTrigger >= -28 & timeBeforeTrigger < 21)) %>%
     ggplot() +
     geom_vline(xintercept = 0) +
@@ -191,7 +122,6 @@ for (exp_name in exp_names_crit) {
     customThemeNoFacet
 
 
-  SAVE <- F
   if (SAVE) {
     ggsave(paste0("trigger_ICU_timeline_", fname, ".png"),
       plot = pplot, path = outdir, width = 12, height = 8, device = "png"
@@ -202,228 +132,204 @@ for (exp_name in exp_names_crit) {
         plot = pplot, path = outdir, width = 12, height = 8, device = "pdf"
       )
     }
-
-
-    separateTrajectories <- F
-    if (separateTrajectories) {
-      library(dplyr)
-      library(zoo)
-
-
-      for (reg in c(1:11)) {
-        for (samp in unique(subdat$sample_num)) {
-          reg <- 11
-          testdat <- subdat %>%
-            filter(region == reg) %>%
-            filter(sample_num %in% c(samp)) %>%
-            filter((timeBeforeTrigger >= -28 & timeBeforeTrigger <= -21)) %>%
-            arrange(region, scen_num, sample_num, date) # %>%
-          # mutate(critical_det=rollapply(critical_det,7,mean,align='right',fill=NA))
-
-          testdat2 <- subdat %>%
-            filter(region == reg) %>%
-            filter(sample_num %in% c(samp)) %>%
-            arrange(region, scen_num, sample_num, date) # %>%
-          # mutate(critical_det=rollapply(critical_det,7,mean,align='right',fill=NA))
-
-
-          pplot <- ggplot(data = testdat) +
-            geom_line(data = testdat2, aes(
-              x = date, y = critical_det,
-              group = capacity_multiplier_fct
-            ), col = "grey", stat = "identity", size = 1.3) +
-            geom_area(data = testdat2, aes(
-              x = date, y = critical_det,
-              group = capacity_multiplier_fct
-            ), fill = "grey", stat = "identity", alpha = 0.3, size = 1.3) +
-            geom_vline(xintercept = c(-Inf, Inf)) +
-            # geom_hline(yintercept = c(-Inf)) +
-            geom_hline(aes(yintercept = icu_available), linetype = "dashed") +
-            geom_hline(aes(yintercept = icu_available * 0.875), linetype = "dashed") +
-            geom_hline(aes(yintercept = icu_available * 0.75), linetype = "dashed") +
-            geom_hline(aes(yintercept = icu_available * 0.625), linetype = "dashed") +
-            geom_hline(aes(yintercept = icu_available * 0.5), linetype = "dashed") +
-            geom_vline(data = testdat2, aes(xintercept = triggerDate, group = capacity_multiplier_fct), col = "black", stat = "identity") +
-            geom_line(aes(
-              x = date, y = critical_det, col = as.factor(capacity_multiplier_fct),
-              group = capacity_multiplier
-            ), stat = "identity", size = 1.3) +
-            facet_wrap(~capacity_multiplier_fct, ncol = 1, strip.position = "right") +
-            labs(color = "Capacity thresholds") +
-            scale_color_viridis_d(direction = -1) +
-            customThemeNoFacet
-
-          ggsave(paste0("trigger_ICU_timeline_", fname, "_sample_", samp, ".png"),
-            plot = pplot, path = file.path(outdir, paste0("covid_region_", reg, "_samples")), width = 12, height = 8, device = "png"
-          )
-        }
-      }
-    }
   }
 
 
+  return(pplot)
+}
+
+f_plot_separateTrajectories <- function(df, reg = 1, samples = NULL, SAVE = T) {
+  library(dplyr)
+  library(zoo)
+
+  if (is.null(samples)) samples <- unique(df$sample_num)
+
+  for (samp in samples) {
+    testdat <- subdat %>%
+      filter(region == reg) %>%
+      filter(sample_num %in% c(samp)) %>%
+      filter((timeBeforeTrigger >= -28 & timeBeforeTrigger <= -21)) %>%
+      arrange(region, scen_num, sample_num, date) # %>%
+    # mutate(critical_det=rollapply(critical_det,7,mean,align='right',fill=NA))
+
+    testdat2 <- subdat %>%
+      filter(region == reg) %>%
+      filter(sample_num %in% c(samp)) %>%
+      arrange(region, scen_num, sample_num, date) # %>%
+    # mutate(critical_det=rollapply(critical_det,7,mean,align='right',fill=NA))
 
 
+    pplot <- ggplot(data = testdat) +
+      geom_line(data = testdat2, aes(
+        x = date, y = critical_det,
+        group = capacity_multiplier_fct
+      ), col = "grey", stat = "identity", size = 1.3) +
+      geom_area(data = testdat2, aes(
+        x = date, y = critical_det,
+        group = capacity_multiplier_fct
+      ), fill = "grey", stat = "identity", alpha = 0.3, size = 1.3) +
+      geom_vline(xintercept = c(-Inf, Inf)) +
+      # geom_hline(yintercept = c(-Inf)) +
+      geom_hline(aes(yintercept = icu_available), linetype = "dashed") +
+      geom_hline(aes(yintercept = icu_available * 0.875), linetype = "dashed") +
+      geom_hline(aes(yintercept = icu_available * 0.75), linetype = "dashed") +
+      geom_hline(aes(yintercept = icu_available * 0.625), linetype = "dashed") +
+      geom_hline(aes(yintercept = icu_available * 0.5), linetype = "dashed") +
+      geom_vline(data = testdat2, aes(xintercept = triggerDate, group = capacity_multiplier_fct), col = "black", stat = "identity") +
+      geom_line(aes(
+        x = date, y = critical_det, col = as.factor(capacity_multiplier_fct),
+        group = capacity_multiplier
+      ), stat = "identity", size = 1.3) +
+      facet_wrap(~capacity_multiplier_fct, ncol = 1, strip.position = "right") +
+      labs(color = "Capacity thresholds") +
+      scale_color_viridis_d(direction = -1) +
+      customThemeNoFacet
+
+    if (SAVE) {
+      ggsave(paste0("trigger_ICU_timeline_", fname, "_sample_", samp, ".png"),
+        plot = pplot, path = file.path(outdir, paste0("covid_region_", reg, "_samples")), width = 12, height = 8, device = "png"
+      )
+    }
+  }
+}
+
+
+f_slope_trigger_plot <- function(df, selected_exps = exp_names_crit, SAVE = T) {
+  library(tidyverse)
   library(lubridate)
-  library(tidyverse) # for purrr, tidyr and dplyr
   library(broom)
 
-  subdat <- df.exp # %>% as.data.frame() %>% filter(exp_name  %in% selectedEXP)
-  table(subdat$exp_name)
 
+  subdat <- subset(df, exp_name %in% selected_exps)
+  table(subdat$exp_name)
 
   reg <- c(1:11)
   testdatCapacity <- subdat %>%
     filter(region %in% reg) %>%
     filter(trigger == 1) %>%
-    group_by(region, scen_num, sample_num, capacity_multiplier) %>%
+    group_by(region, scen_num, sample_num, capacity_multiplier, exp_name) %>%
     filter(critical_det == max(critical_det)) %>%
     filter(date == min(date)) %>%
     mutate(aboveCapacity = ifelse(critical_det > icu_available, "yes", "no")) %>%
-    select(region, scen_num, sample_num, capacity_multiplier, aboveCapacity)
+    select(region, scen_num, sample_num, capacity_multiplier, aboveCapacity, exp_name)
 
 
   testdat <- subdat %>%
     filter(region %in% reg) %>%
     filter(trigger == 1) %>%
-    group_by(region, scen_num, sample_num, capacity_multiplier) %>%
+    group_by(region, scen_num, sample_num, capacity_multiplier, exp_name) %>%
     filter((timeBeforeTrigger >= -28 & timeBeforeTrigger <= -21)) %>%
-    group_by(region, scen_num, sample_num, capacity_multiplier) %>%
+    group_by(region, scen_num, sample_num, capacity_multiplier, exp_name) %>%
     nest() %>%
     mutate(model = map(data, ~ lm(critical_det ~ date, data = .x) %>% tidy())) %>%
     unnest(model) %>%
     filter(term == "date")
 
+  testdat <- subdat %>%
+    filter(region %in% reg) %>%
+    filter(trigger == 1) %>%
+    group_by(region, scen_num, sample_num, capacity_multiplier, exp_name) %>%
+    filter((timeBeforeTrigger >= -28 & timeBeforeTrigger <= -21)) %>%
+    group_by(region, scen_num, sample_num, capacity_multiplier, exp_name) %>%
+    nest() %>%
+    mutate(model = map(data, ~ lm(critical_det ~ date, data = .x) %>% tidy())) %>%
+    unnest(model) %>%
+    filter(term == "date")
 
-  testdat <- testdat %>% left_join(testdatCapacity, by = c("region", "scen_num", "sample_num", "capacity_multiplier"))
+  testdat <- testdat %>% left_join(testdatCapacity, by = c("region", "scen_num", "sample_num", "capacity_multiplier", "exp_name"))
 
 
   pplot <- ggplot(data = testdat) +
-    geom_vline(xintercept = 0) +
-    geom_point(aes(x = estimate, y = capacity_multiplier, col = as.factor(aboveCapacity), group = aboveCapacity), position = "dodge") +
+    geom_hline(yintercept = 0) +
+    # geom_boxplot(aes(y = estimate, x = capacity_multiplier, col = as.factor(aboveCapacity), group = interaction(capacity_multiplier, aboveCapacity)), position = "dodge") +
+    geom_point(aes(y = estimate, x = capacity_multiplier, col = as.factor(aboveCapacity), group = interaction(aboveCapacity, exp_name)), position = "dodge") +
+    #    geom_point(aes(y= estimate, x = capacity_multiplier, col = as.factor(aboveCapacity),shape=exp_name, group = interaction(aboveCapacity, exp_name)), position = "dodge") +
+    coord_flip() +
     labs(
-      title = reopenLabel,
+      title = "all reopening scenarios",
       subtitle = "",
-      x = "slope of critical detected 3 weeks preceeding capacity trigger",
-      y = "capacity trigger",
+      y = "slope of critical detected 3 weeks preceeding capacity trigger",
+      x = "capacity trigger",
       color = "above capacity"
     ) +
     facet_wrap(~region, scales = "free_x") +
     customThemeNoFacet +
     scale_color_manual(values = c("deepskyblue3", "orange"))
 
+  if (SAVE) {
+    fname <- "all_scens"
+    ggsave(paste0("slope_trigger_capacity_", fname, ".png"),
+      plot = pplot, path = file.path(outdir), width = 12, height = 8, device = "png"
+    )
+  }
 
-
-  ggsave(paste0("slope_trigger_capacity_", fname, ".png"),
-    plot = pplot, path = file.path(outdir), width = 12, height = 8, device = "png"
-  )
+  return(pplot)
 }
 
 
-###
-
-# subdat <-
-subdat %>%
-  filter(sample_num %in% c(0)) %>%
-  filter((timeBeforeTrigger >= -21 & timeBeforeTrigger < 21)) %>%
-  ggplot() +
-  geom_vline(xintercept = 0) +
-  geom_hline(aes(yintercept = icu_available), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.875), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.75), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.625), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.5), linetype = "dashed") +
-  geom_line(aes(x = timeBeforeTrigger, y = critical_det, col = capacity_multiplier_fct, group = interaction(scen_num, capacity_multiplier_fct)), alpha = 0.9, size = 1) +
-  facet_wrap(~region, scales = "free") +
-  labs(
-    title = reopenLabel,
-    subtitle = "",
-    color = "Capacity threshold",
-    x = "Time relative to Trigger"
-  ) +
-  scale_color_viridis_d(direction = -1) +
-  customThemeNoFacet
+## ----------------------
+### Generate plots
+## ----------------------
+exp_names <- list.dirs(simulation_output, recursive = FALSE, full.names = FALSE)
 
 
+exp_names_crit <- exp_names[grep("crit", exp_names)]
+exp_names_hosp <- exp_names[grep("hosp", exp_names)]
 
 
-subdat %>%
-  filter(region == 1) %>%
-  filter(sample_num %in% c(0:10)) %>%
-  filter((timeBeforeTrigger >= -21 & timeBeforeTrigger < 21)) %>%
-  ggplot() +
-  geom_vline(xintercept = 0) +
-  geom_hline(aes(yintercept = icu_available), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.875), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.75), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.625), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.5), linetype = "dashed") +
-  geom_line(aes(x = timeBeforeTrigger, y = critical_det, col = capacity_multiplier_fct, group = interaction(scen_num, capacity_multiplier_fct)), alpha = 0.9, size = 1) +
-  facet_wrap(~sample_num, scales = "free") +
-  labs(
-    title = reopenLabel,
-    subtitle = "Covid region 1, 10 samples",
-    color = "Capacity threshold",
-    x = "Time relative to Trigger"
-  ) +
-  scale_color_viridis_d(direction = -1) +
-  customThemeNoFacet
+## -------------------------------
+### COmbine data
+## -------------------------------
+datList <- list()
+for (exp_name in exp_names_crit) {
+
+  # exp_name <- exp_names[2]
+  print(exp_name)
+
+  exp_dir <- file.path(file.path(simulation_output, exp_name))
+  if (sum(grep("crit", exp_name)) == 1) expLabel <- "critical"
+  if (sum(grep("hosp", exp_name)) == 1) expLabel <- "hospitalized"
+
+  df.exp <- f_loadDat(exp_name)
+  datList[[length(datList) + 1]] <- df.exp
+
+  rm(exp_name)
+}
+df.exp <- datList %>% bind_rows(.id = "exp_nr")
 
 
+## -------------------------------
+## Generate plots
+## -------------------------------
+for (exp_name in exp_names_crit) {
 
-pplotdat <- subdat %>%
-  filter(region == 1) %>%
-  filter((timeBeforeTrigger >= -21 & timeBeforeTrigger < 21))
+  # exp_name <- exp_names_crit[2]
+  print(exp_name)
 
-pplotdat2 <- subdat %>%
-  filter(region == 1)
+  fname <- gsub("20200820_IL_critdet_", "", exp_name)
+  fname <- gsub("20200821_IL_critdet_", "", fname)
+  fname <- gsub("_TriggeredRollback", "", fname)
 
+  reopenLabel <- paste0(gsub("perc", "", gsub("reopen", "", fname)), "% reopening scenario")
 
-pplotdat3 <- subdat %>%
-  filter(region == 1) %>%
-  group_by(capacity_multiplier_fct) %>%
-  summarize(triggerDate = mean(triggerDate, na.rm = TRUE))
+  selectedEXP <- exp_name
+  subdat <- df.exp %>%
+    as.data.frame() %>%
+    filter(exp_name %in% selectedEXP)
 
+  ### Descriptives
+  table(subdat$exp_name)
+  summary(subdat$date)
+  tapply(subdat$timeBeforeTrigger, subdat$trigger, summary)
+  table(subdat$trigger, subdat$region)
+  table(subdat$trigger, subdat$changedKi)
 
-ggplot(data = pplotdat) +
-  geom_vline(data = pplotdat3, aes(xintercept = triggerDate, col = capacity_multiplier_fct)) +
-  geom_hline(aes(yintercept = icu_available), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.875), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.75), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.625), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.5), linetype = "dashed") +
-  geom_line(data = pplotdat2, aes(x = date, y = critical_det, group = interaction(scen_num, capacity_multiplier_fct)), col = "grey", alpha = 0.3) +
-  #  geom_smooth(data=pplotdat2aes(x = date, y = critical_det, group = interaction(capacity_multiplier_fct), col = as.factor(capacity_multiplier_fct))) +
-  geom_line(aes(x = date, y = critical_det, group = interaction(scen_num, capacity_multiplier_fct)), col = "grey", alpha = 0.3) +
-  geom_smooth(aes(x = date, y = critical_det, group = interaction(capacity_multiplier_fct), col = as.factor(capacity_multiplier_fct))) +
-  # facet_wrap(~timeBeforeTrigger, scales = "free", ncol=1) +
-  labs(
-    title = reopenLabel,
-    subtitle = "",
-    color = "Capacity threshold",
-    x = "Time relative to Trigger"
-  ) +
-  scale_color_viridis_d(direction = -1) +
-  customThemeNoFacet
+  f_plot_prepostTrigger(subdat)
 
+  for (reg in c(1:11)) {
+    f_plot_separateTrajectories()
+  }
+}
 
-ggplot(data = pplotdat) +
-  geom_vline(data = pplotdat3, aes(xintercept = triggerDate, col = capacity_multiplier_fct)) +
-  geom_hline(aes(yintercept = icu_available), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.875), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.75), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.625), linetype = "dashed") +
-  geom_hline(aes(yintercept = icu_available * 0.5), linetype = "dashed") +
-  geom_line(data = pplotdat2, aes(x = date, y = critical_det, group = interaction(scen_num, capacity_multiplier_fct)), col = "grey", alpha = 0.3) +
-  #  geom_smooth(data=pplotdat2aes(x = date, y = critical_det, group = interaction(capacity_multiplier_fct), col = as.factor(capacity_multiplier_fct))) +
-  geom_line(aes(x = date, y = critical_det, col = as.factor(capacity_multiplier_fct), group = interaction(scen_num, capacity_multiplier_fct)), alpha = 0.5) +
-  # geom_smooth(aes(x = date, y = critical_det, group = interaction(capacity_multiplier_fct), col = as.factor(capacity_multiplier_fct))) +
-  # facet_wrap(~timeBeforeTrigger, scales = "free", ncol=1) +
-  labs(
-    title = reopenLabel,
-    subtitle = "",
-    color = "Capacity threshold",
-    x = "Time relative to Trigger"
-  ) +
-  scale_color_viridis_d(direction = -1) +
-  customThemeNoFacet
+f_slope_trigger_plot(df = df.exp, selected_exps = exp_names_crit, SAVE = F)
+
