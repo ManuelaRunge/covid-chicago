63b3363af4b53d7e87484f87c12bca9e5043ec0c
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-26 22:16:58 -0500

    updated Rt combine

--- Rfiles/estimate_Rt/combine_Rt.R
+++ Rfiles/estimate_Rt/combine_Rt.R
@@ -1,36 +1,33 @@
 ## ============================================================
-## R script to combine Rt files 
+## R script to combine Rt files
 ## ============================================================
 
 library(tidyverse)
 
-f_loadAndCombine <- function(filedir, filename, identfier ){
-
-  load(file.path(filedir, paste0(identfier[1],filename,".Rdata")))
+f_loadAndCombine <- function(filedir, filename, identfier) {
+  load(file.path(filedir, paste0(identfier[1], filename, ".Rdata")))
   Rt_dat <- Rt_tempdat_All
-  
-  for (i in  identfier ) {
+
+  for (i in identfier) {
     load(file.path(filedir, paste0(i, filename, ".Rdata")))
     Rt_dat <- rbind(Rt_dat, Rt_tempdat_All)
     rm(Rt_tempdat_All)
   }
- return(Rt_dat) 
+  return(Rt_dat)
 }
 
 
 ## Load directories and custom objects and functions
-if(!exists("Location"))Location = "LOCAL"
-if(Location == "NUCLUSTER") setwd("/home/mrm9534/gitrepos/covid-chicago/Rfiles/")
+if (!exists("Location")) Location <- "LOCAL"
+if (Location == "NUCLUSTER") setwd("/home/mrm9534/gitrepos/covid-chicago/Rfiles/")
 source("load_paths.R")
 
-if(!exists("exp_name"))exp_name ="20200816_IL_testbaseline"
-fname = "_temp_Rt_tempdat_All"
-   
-Rt_dir <- file.path(simulation_output, exp_name,  "estimatedRt")
+if (!exists("exp_name")) exp_name <- "20200825_IL_RR_baseline_0"
+fname <- "_estimated_Rt"
 
-Rt_dat <- f_loadAndCombine(filedir=Rt_dir, filename=fname, identfier=c(1:11))
+Rt_dir <- file.path(simulation_output, exp_name, "estimatedRt")
 
-save(Rt_dat, file = file.path(Rt_dir, paste0("combined",fname, ".Rdata")))
-write.csv(Rt_dat, file = file.path(Rt_dir, paste0("combined",fname, ".csv")), row.names = FALSE)
-   
+Rt_dat <- f_loadAndCombine(filedir = Rt_dir, filename = fname, identfier = c(1:11))
 
+save(Rt_dat, file = file.path(Rt_dir, paste0("combined", fname, ".Rdata")))
+write.csv(Rt_dat, file = file.path(Rt_dir, paste0("combined", fname, ".csv")), row.names = FALSE)
