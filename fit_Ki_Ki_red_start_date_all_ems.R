48a5afba5650f36153fb157b56f55d35cd98ae24
ManuelaRunge <manuela.runge@northwestern.edu>
2020-09-25 19:30:03 -0500

    improved speed and readibility in fitting plot, updated data sources

--- fit_Ki_Ki_red_start_date_all_ems.R
+++ fit_Ki_Ki_red_start_date_all_ems.R
@@ -18,6 +18,7 @@
 ## set working directory to R.project location
 library(here)
 library(ggplot2)
+library(data.table)
 library(tidyverse)
 
 
@@ -36,24 +37,18 @@ if(username=="mrung"){
 simdate = "20200525"
 
 ## Read in simulations, put in list
-sim_ems1 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_1_fit_test1/trajectoriesDat.csv")))
-sim_ems2 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_2_fit_test1/trajectoriesDat.csv")))
-sim_ems3 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_3_fit_test1/trajectoriesDat.csv")))
-sim_ems4 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_4_fit_test1/trajectoriesDat.csv")))
-sim_ems5 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_5_fit_test1/trajectoriesDat.csv")))
-sim_ems6 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_6_fit_test1/trajectoriesDat.csv")))
-sim_ems7 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_7_fit_test1/trajectoriesDat.csv")))
-sim_ems8 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_8_fit_test1/trajectoriesDat.csv")))
-sim_ems9 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_9_fit_test1/trajectoriesDat.csv")))
-sim_ems10 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_10_fit_test1/trajectoriesDat.csv")))
-sim_ems11 <- read.csv(file.path(simulation_output,"forFitting", paste0(simdate,"_EMS_11_fit_test1/trajectoriesDat.csv")))
+exp_names <- list.dirs(file.path(simulation_output,"forFitting"), recursive = FALSE, full.names = FALSE)
+exp_names <- exp_names[grep("_test1",exp_names)]
 
+simulationslist <- list()
+for(exp_name in exp_names){
+  simulationslist[[length(simulationslist)+1]] <- fread(file.path(simulation_output,"forFitting", exp_name,"trajectoriesDat.csv" ))
+}
 
-simulationslist <- list(sim_ems1, sim_ems2, sim_ems3, sim_ems4, sim_ems5, sim_ems6, sim_ems7, sim_ems8, sim_ems9, sim_ems10, sim_ems11)
 # simulationslist <- list(sim_ems2)
 
 # read in hospital data
-hosp_data <- read.csv(file.path(data_dir,"covid_IDPH/Corona virus reports/emresource_by_region.csv"))
+hosp_data <- fread(file.path(data_path,"covid_IDPH/Corona virus reports/emresource_by_region.csv")) 
 
 # Replace NA values with zeros (blank in raw data sheet)
 hosp_data[is.na(hosp_data)] <- 0
@@ -62,79 +57,40 @@ hosp_data[is.na(hosp_data)] <- 0
 hosp_data$date_of_extract <- as.Date(hosp_data$date_of_extract)
 
 ## Separate hospital data by EMS, put in list
-hosp_data_ems1 <- hosp_data[which(hosp_data$region == 1), ]
-hosp_data_ems2 <- hosp_data[which(hosp_data$region == 2), ]
-hosp_data_ems3 <- hosp_data[which(hosp_data$region == 3), ]
-hosp_data_ems4 <- hosp_data[which(hosp_data$region == 4), ]
-hosp_data_ems5 <- hosp_data[which(hosp_data$region == 5), ]
-hosp_data_ems6 <- hosp_data[which(hosp_data$region == 6), ]
-hosp_data_ems7 <- hosp_data[which(hosp_data$region == 7), ]
-hosp_data_ems8 <- hosp_data[which(hosp_data$region == 8), ]
-hosp_data_ems9 <- hosp_data[which(hosp_data$region == 9), ]
-hosp_data_ems10 <- hosp_data[which(hosp_data$region == 10), ]
-hosp_data_ems11 <- hosp_data[which(hosp_data$region == 11), ]
-
-hosp_list <- list(hosp_data_ems1,hosp_data_ems2,hosp_data_ems3, hosp_data_ems4, hosp_data_ems5, hosp_data_ems6, hosp_data_ems7, hosp_data_ems8, hosp_data_ems9, hosp_data_ems10, hosp_data_ems11)
+hosp_list <- list()
+for(i in c(1:11)){
+  hosp_list[[length(hosp_list)+1]] <- hosp_data[hosp_data$covid_region == i,]
+  
+}
 
 # Read in by line hospital admissions data
-pre_hosp_adm_data <- read.csv(file.path(data_dir, "covid_IDPH", "Cleaned Data", "200602_jg_admission_date_ems.csv"))
-
+pre_hosp_adm_data <- fread(file.path(data_path, "covid_IDPH", "Cleaned Data", "200921_jg_admission_date_covidregion.csv"))
 
 # Make date column as date
 pre_hosp_adm_data <- pre_hosp_adm_data %>%
   mutate(date = as.Date(date, "%Y-%m-%d"))
 
 # Divide by EMS and put in list
-pre_hosp_adm_data_ems1 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 1), ]
-pre_hosp_adm_data_ems2 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 2), ]
-pre_hosp_adm_data_ems3 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 3), ]
-pre_hosp_adm_data_ems4 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 4), ]
-pre_hosp_adm_data_ems5 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 5), ]
-pre_hosp_adm_data_ems6 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 6), ]
-pre_hosp_adm_data_ems7 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 7), ]
-pre_hosp_adm_data_ems8 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 8), ]
-pre_hosp_adm_data_ems9 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 9), ]
-pre_hosp_adm_data_ems10 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 10), ]
-pre_hosp_adm_data_ems11 <- pre_hosp_adm_data[which(pre_hosp_adm_data$EMS == 11), ]
+pre_hosp_adm_list <- list()
+for(i in c(1:11)){
+  pre_hosp_adm_list[[length(pre_hosp_adm_list)+1]] <- pre_hosp_adm_data[pre_hosp_adm_data$covid_region == i,]
+  
+}
 
-pre_hosp_adm_list <- list(
-  pre_hosp_adm_data_ems1, pre_hosp_adm_data_ems2,
-  pre_hosp_adm_data_ems3, pre_hosp_adm_data_ems4,
-  pre_hosp_adm_data_ems5, pre_hosp_adm_data_ems6,
-  pre_hosp_adm_data_ems7, pre_hosp_adm_data_ems8,
-  pre_hosp_adm_data_ems9, pre_hosp_adm_data_ems10,
-  pre_hosp_adm_data_ems11
-)
 # pre_hosp_adm_list <- list(pre_hosp_adm_data_ems2)
 
 # Read in by line hospital death data
-pre_hosp_det_data <- read.csv(file.path(data_dir, "covid_IDPH", "Cleaned Data", "200602_jg_deceased_date_ems.csv"))
+pre_hosp_det_data <- fread(file.path(data_path, "covid_IDPH", "Cleaned Data", "200921_jg_deceased_date_covidregion.csv"))
 # Make date column as dat
 pre_hosp_det_data <- pre_hosp_det_data %>%
   mutate(date = as.Date(date, "%Y-%m-%d"))
 
 # Divide by EMS and put in list
-pre_hosp_det_data_ems1 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 1), ]
-pre_hosp_det_data_ems2 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 2), ]
-pre_hosp_det_data_ems3 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 3), ]
-pre_hosp_det_data_ems4 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 4), ]
-pre_hosp_det_data_ems5 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 5), ]
-pre_hosp_det_data_ems6 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 6), ]
-pre_hosp_det_data_ems7 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 7), ]
-pre_hosp_det_data_ems8 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 8), ]
-pre_hosp_det_data_ems9 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 9), ]
-pre_hosp_det_data_ems10 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 10), ]
-pre_hosp_det_data_ems11 <- pre_hosp_det_data[which(pre_hosp_det_data$EMS == 11), ]
-
-pre_hosp_det_list <- list(
-  pre_hosp_det_data_ems1, pre_hosp_det_data_ems2,
-  pre_hosp_det_data_ems3, pre_hosp_det_data_ems4,
-  pre_hosp_det_data_ems5, pre_hosp_det_data_ems6,
-  pre_hosp_det_data_ems7, pre_hosp_det_data_ems8,
-  pre_hosp_det_data_ems9, pre_hosp_det_data_ems10,
-  pre_hosp_det_data_ems11
-)
-
+pre_hosp_det_list <- list()
+for(i in c(1:11)){
+  pre_hosp_det_list[[length(pre_hosp_det_list)+1]] <- pre_hosp_det_data[pre_hosp_det_data$covid_region == i,]
+  
+}
 
 ## loop begin over EMS regions
 use_values_list <- list()
@@ -142,97 +98,89 @@ for (i in 1:11) {
 
   # OK, let's prepare the simulation sheet
   sim_ems <- simulationslist[[i]]
+  colnames(sim_ems) <- gsub(paste0("_EMS-",i),"",colnames(sim_ems))
   sim_ems <- na.omit(sim_ems)
 
 
   # In the simulations, add a colum for deaths each day rather than cumulative deaths
-  sim_ems$death_det_24hr <- 0
-  sim_ems$death_det_24hr[2:length(sim_ems$death_det_24hr)] <- sim_ems$death_det_cumul[2:length(sim_ems$death_det_24hr)] - sim_ems$death_det_cumul[1:(length(sim_ems$death_det_24hr) - 1)]
-  for (x in 1:length(sim_ems$death_det_24hr)) {
-    if (sim_ems$time[x] == 0) {
-      sim_ems$death_det_24hr[x] <- 0
-    }
-  }
+  sim_ems <- sim_ems %>% 
+    group_by(scen_num) %>% 
+    arrange(time) %>% 
+    mutate(death_det_24hr=0, death_det_24hr = death_det_cumul- lag(death_det_cumul)) %>%
+    mutate(death_det_24hr = ifelse(time==0, 0,death_det_24hr ))
 
   # Also add a column for new detected symptomatic each day
-  sim_ems$new_det <- 0
-  sim_ems$new_det[2:length(sim_ems$new_det)] <- (sim_ems$symp_mild_det_cumul[2:length(sim_ems$symp_mild_det_cumul)] + sim_ems$symp_severe_det_cumul[2:length(sim_ems$symp_severe_det_cumul)]) -
-    (sim_ems$symp_mild_det_cumul[1:(length(sim_ems$symp_mild_det_cumul) - 1)] + sim_ems$symp_severe_det_cumul[1:(length(sim_ems$symp_severe_det_cumul) - 1)])
-  for (x in 1:length(sim_ems$new_det)) {
-    if (sim_ems$time[x] == 0) {
-      sim_ems$new_det[x] <- 0
-    }
-  }
-
-  # We need to match simulation dates with real world dates.
-  # simulation dates aren't integers, so round them.
+  sim_ems <- sim_ems %>% 
+    group_by(scen_num) %>% 
+    arrange(time) %>% 
+    mutate(new_det=0, new_det = (symp_mild_det_cumul- lag(symp_mild_det_cumul) + (symp_severe_det_cumul- lag(symp_severe_det_cumul)))) %>%
+    mutate(new_det = ifelse(time==0, 0,new_det ))
+  #We need to match simulation dates with real world dates. 
+  #simulation dates aren't integers, so round them. 
   sim_ems$time <- floor(sim_ems$time)
-  table(sim_ems$startdate)
-
+  
   ## get EMS specific hosp_data
   hosp_data_ems <- hosp_list[[i]]
-
-  ## get EMS specific admissions data
+  
+  ##get EMS specific admissions data 
   pre_hosp_adm_data_ems <- pre_hosp_adm_list[[i]]
-
-  # Fill in dates that are missing with 0's
+  
+  #Fill in dates that are missing with 0's
   pre_hosp_adm_data_ems <- pre_hosp_adm_data_ems %>%
-    complete(date = seq.Date(min(pre_hosp_adm_data_ems$date), max(pre_hosp_adm_data_ems$date), by = "day"))
+    complete(date = seq.Date(min(pre_hosp_adm_data_ems$date), max(pre_hosp_adm_data_ems$date), by="day"))
   pre_hosp_adm_data_ems$EMS <- i
-  pre_hosp_adm_data_ems[is.na(pre_hosp_adm_data_ems)] <- 0
-
-  ## get EMS specific death data
+  pre_hosp_adm_data_ems[is.na(pre_hosp_adm_data_ems)] = 0
+  
+  ##get EMS specific death data 
   pre_hosp_det_data_ems <- pre_hosp_det_list[[i]]
-
-  # Fill in dates that are missing with 0's
+  
+  #Fill in dates that are missing with 0's
   pre_hosp_det_data_ems <- pre_hosp_det_data_ems %>%
-    complete(date = seq.Date(min(pre_hosp_det_data_ems$date), max(pre_hosp_det_data_ems$date), by = "day"))
+    complete(date = seq.Date(min(pre_hosp_det_data_ems$date), max(pre_hosp_det_data_ems$date), by="day"))
   pre_hosp_det_data_ems$EMS <- i
-  pre_hosp_det_data_ems[is.na(pre_hosp_det_data_ems)] <- 0
-
-
-  ## get a list of all scenario numbers run for this EMS
+  pre_hosp_det_data_ems[is.na(pre_hosp_det_data_ems)] = 0
+  
+  
+  ##get a list of all scenario numbers run for this EMS
   scens <- unique(sim_ems$scen_num)
-
-  # create data.frame to hold likelihood results
-  ems_output <- matrix(0, length(scens), 4)
+  
+  #create data.frame to hold likelihood results
+  ems_output <- matrix(0,length(scens),4)
   ems_output <- as.data.frame(ems_output)
-  colnames(ems_output) <- c("Start_date", "Ki", "Ki_red", "NLL")
-  # ems_output$Start_date <- as.Date(ems_output$Start_date, origin = '2020-01-01')
-  ems_output$Start_date <- "" # as.Date(sim_ems$startdate, format="yyyy-mm-")
+  colnames(ems_output) <- c('Start_date','Ki','Ki_red','NLL')
+  ems_output$Start_date <- as.Date(ems_output$Start_date, origin = '2020-01-01')
 
 
   # loop over simulation scenarios and record likelihood
-  
+  index = 1
   for (j in 1:length(scens)) {
     # pull out all simulation values of given Ki value
-    sim_ems_subset <- sim_ems[which(sim_ems$scen_num == scens[j]), ]
-    # Set the start date to be that based on when we started disease interventions
-    # sim_ems_subset$time <- as.Date(sim_ems_subset$time,origin = (as.Date("2020-03-12") - floor(sim_ems_subset$socialDistance_time1[1])))
-    sim_ems_subset$time <- as.Date(sim_ems_subset$startdate)
-
-    # Get earliest and latest dates of the simualtion data and hopital date
-    mindate <- max(min(sim_ems_subset$time), min(hosp_data_ems$date_of_extract))
-    maxdate <- min(max(sim_ems_subset$time), max(hosp_data_ems$date_of_extract))
-
-    # get rid of dates that don't overlap for comparison'
-    sim_ems_subset1 <- sim_ems_subset # sim_ems_subset[which(sim_ems_subset$time >= mindate & sim_ems_subset$time <= maxdate),]
-    hosp_data_ems_subset <- hosp_data_ems # hosp_data_ems[which(hosp_data_ems$date_of_extract >= mindate & hosp_data_ems$date_of_extract <= maxdate),]
-
-    # Repeat process for simulation data and hosp admission data
-    mindate <- max(min(sim_ems_subset$time), min(pre_hosp_adm_data_ems$date))
-    maxdate <- min(max(sim_ems_subset$time), max(pre_hosp_adm_data_ems$date))
-
-    sim_ems_subset2 <- sim_ems_subset # sim_ems_subset[which(sim_ems_subset$time >= mindate & sim_ems_subset$time <= maxdate),]
-    pre_hosp_adm_data_ems_subset <- pre_hosp_adm_data_ems # pre_hosp_adm_data_ems[which(pre_hosp_adm_data_ems$date >= mindate & pre_hosp_adm_data_ems$date <= maxdate),]
-
-    # Repeat process for simulation data and hosp death data
-    mindate <- max(min(sim_ems_subset$time), min(pre_hosp_det_data_ems$date))
-    maxdate <- min(max(sim_ems_subset$time), max(pre_hosp_det_data_ems$date))
-
-    sim_ems_subset3 <- sim_ems_subset # sim_ems_subset[which(sim_ems_subset$time >= mindate & sim_ems_subset$time <= maxdate),]
-    pre_hosp_det_data_ems_subset <- pre_hosp_det_data_ems # pre_hosp_det_data_ems[which(pre_hosp_det_data_ems$date >= mindate & pre_hosp_det_data_ems$date <= maxdate),]
-
+    sim_ems_subset <- sim_ems[which(sim_ems$scen_num == scens[j]),]
+    #Set the start date to be that based on when we started disease interventions
+    sim_ems_subset$time <- as.Date(sim_ems_subset$time,origin = (as.Date("2020-03-12") - floor(sim_ems_subset$socialDistance_time1[1])))
+    
+    #Get earliest and latest dates of the simualtion data and hopital date
+    mindate <- max(min(sim_ems_subset$time),min(hosp_data_ems$date_of_extract))
+    maxdate <- min(max(sim_ems_subset$time),max(hosp_data_ems$date_of_extract))
+    
+    #get rid of dates that don't overlap for comparison'
+    sim_ems_subset1 <- sim_ems_subset[which(sim_ems_subset$time >= mindate & sim_ems_subset$time <= maxdate),]
+    hosp_data_ems_subset <- hosp_data_ems[which(hosp_data_ems$date_of_extract >= mindate & hosp_data_ems$date_of_extract <= maxdate),]
+    
+    #Repeat process for simulation data and hosp admission data
+    mindate <- max(min(sim_ems_subset$time),min(pre_hosp_adm_data_ems$date))
+    maxdate <- min(max(sim_ems_subset$time),max(pre_hosp_adm_data_ems$date))
+    
+    sim_ems_subset2 <- sim_ems_subset[which(sim_ems_subset$time >= mindate & sim_ems_subset$time <= maxdate),]
+    pre_hosp_adm_data_ems_subset <- pre_hosp_adm_data_ems[which(pre_hosp_adm_data_ems$date >= mindate & pre_hosp_adm_data_ems$date <= maxdate),]
+    
+    #Repeat process for simulation data and hosp death data
+    mindate <- max(min(sim_ems_subset$time),min(pre_hosp_det_data_ems$date))
+    maxdate <- min(max(sim_ems_subset$time),max(pre_hosp_det_data_ems$date))
+    
+    sim_ems_subset3 <- sim_ems_subset[which(sim_ems_subset$time >= mindate & sim_ems_subset$time <= maxdate),]
+    pre_hosp_det_data_ems_subset <- pre_hosp_det_data_ems[which(pre_hosp_det_data_ems$date >= mindate & pre_hosp_det_data_ems$date <= maxdate),]
+    
 
     # Calculate likelyhood
     # Likelyhood of simulation generating detected critical
@@ -247,9 +195,9 @@ for (i in 1:11) {
     nll <- nll1 + nll2 + (nll3 + nll4) * (length(hosp_data_ems$confirmed_covid_icu) / length(pre_hosp_adm_data_ems$cases)) / 2
 
     # Put likelihood values and corresponding start date and Ki in output dataframe
-    ems_output$Start_date[j] <- as.character(unique(sim_ems_subset$startdate)) # as.Date((as.Date("2020-03-12") - floor(sim_ems_subset$socialDistance_time1[1])))
+    ems_output$time_infection_import[j] <- as.character(unique(sim_ems_subset$time_infection_import)) # as.Date((as.Date("2020-03-12") - floor(sim_ems_subset$socialDistance_time1[1])))
     ems_output$Ki[j] <- sim_ems_subset$Ki[1]
-    ems_output$Ki_red[j] <- sim_ems_subset$social_multiplier_4[1]
+    #ems_output$Ki_red[j] <- sim_ems_subset$social_multiplier_4[1]
     ems_output$NLL[j] <- nll
 
   }
@@ -298,6 +246,9 @@ f_paramplots <- function(use_values_dat, ems) {
     xlim(0, 1) +
     labs(title = "Ki_Ki_red", subtitle = ems, x = "\n\n\nKi")
 
+  
+  if(!dir.exists(file.path(out_dir,"best_parameter_plots/")))dir.create(file.path(out_dir,"best_parameter_plots/"))
+  
   pplot <- plot_grid(Ki_startdate, Ki_red_startdate, Ki_Ki_red, nrow = 1)
   ggsave(paste0(ems, "_best_parameter_plots.png"),
     plot = pplot,
@@ -316,7 +267,8 @@ for (i in c(1:11)) {
   f_paramplots(use_values_dat = nam, ems = ems_i)
 
   # Save best paramter combinations
-  write.csv(nam, paste0(out_dir,"best_parameter_sheets/best_parameter_ranges_ems", i, ".csv"))
+  if(!dir.exists(file.path(out_dir,"best_parameter_sheets/")))dir.create(file.path(out_dir,"best_parameter_sheets/"))
+  write.csv(nam, file.path(out_dir,"best_parameter_sheets/best_parameter_ranges_ems", i, ".csv"))
 }
 
 
