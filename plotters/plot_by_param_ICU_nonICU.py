0360ff7ded51b3064a8a301a01f55b8e96099267
ManuelaRunge <manuela.runge@northwestern.edu>
2020-10-22 06:05:29 -0500

    added plot hosp_det and crit_det for one region

--- plotters/plot_by_param_ICU_nonICU.py
+++ plotters/plot_by_param_ICU_nonICU.py
@@ -15,8 +15,8 @@ mpl.rcParams['pdf.fonttype'] = 42
 datapath, projectpath, wdir,exe_dir, git_dir = load_box_paths()
 
 first_day = date(2020, 2, 13) # IL
-first_plot_day = date(2020, 7, 15)
-last_plot_day = date(2020, 10, 15)
+first_plot_day = date(2020, 8, 1)
+last_plot_day = date(2020,12, 30)
 
 
 def load_sim_data(exp_name, region_suffix ='_All', input_wdir=None,fname='trajectoriesDat_trim.csv', input_sim_output_path =None) :
@@ -54,13 +54,50 @@ def plot_on_fig(df, c, axes,channel, color,panel_heading, ems, label=None, addgr
 
     if channel=="hosp_det":
         datachannel = 'covid_non_icu'
+        capacitychannel = 'hospitalized'
     if channel=="crit_det":
         datachannel = 'confirmed_covid_icu'
+        capacitychannel = 'critical'
 
     ax.plot(ref_df['date'], ref_df[datachannel], 'o', color='#303030', linewidth=0, ms=3)
     ax.plot(ref_df['date'], ref_df[datachannel].rolling(window=7, center=True).mean(), c='k', alpha=1.0)
-   #ax.set_yscale('log')
-   # ax.set_ylim(0, max(mdf['CI_75']))
+
+    capacity = load_capacity(ems)
+    ax.plot([np.min(mdf['date']), np.max(mdf['date'])],
+                [capacity[capacitychannel], capacity[capacitychannel]], '--', linewidth=1, color='black')
+
+def plot_on_fig2(df, axes,  ems_nr, label=None, addgrid=True) :
+    palette = sns.color_palette('Set1', 2)
+    for c, channel in enumerate(['hosp_det','crit_det']):
+        ax = axes[c]
+        df['date'] = df['time'].apply(lambda x: first_day + timedelta(days=int(x)))
+        df = df[(df['date'] >= first_plot_day) & (df['date'] <= last_plot_day)]
+        mdf = df.groupby('date')[channel].agg([CI_50, CI_2pt5, CI_97pt5, CI_25, CI_75]).reset_index()
+
+        if addgrid ==True : ax.grid(b=True, which='major', color='#999999', linestyle='-', alpha=0.3)
+        ax.plot(mdf['date'], mdf['CI_50'], color=palette[0], label=label)
+        ax.fill_between(mdf['date'].values, mdf['CI_25'], mdf['CI_75'], color=palette[0], linewidth=0, alpha=0.4)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d\n%b'))
+
+        ref_df = compare_ems(ems=ems_nr, channel=channel)
+
+        if channel=="hosp_det":
+            datachannel = 'covid_non_icu'
+            capacitychannel = 'hospitalized'
+            channel_label = 'hospital census'
+        if channel=="crit_det":
+            datachannel = 'confirmed_covid_icu'
+            capacitychannel = 'critical'
+            channel_label = 'intensive case unit census'
+
+        ax.plot(ref_df['date'], ref_df[datachannel], 'o', color='#303030', linewidth=0, ms=3)
+        ax.plot(ref_df['date'], ref_df[datachannel].rolling(window=7, center=True).mean(), c='k', alpha=1.0)
+
+        capacity = load_capacity(ems_nr)
+        ax.plot([np.min(mdf['date']), np.max(mdf['date'])],
+                    [capacity[capacitychannel], capacity[capacitychannel]], '--', linewidth=1, color='black')
+        ax.set_title('capacity limit', y=0.86)
+        ax.set_ylabel(f'predicted {channel_label}')
 
 def compare_ems( ems,channel):
     ref_df = pd.read_csv(os.path.join(datapath, 'covid_IDPH', 'Corona virus reports', 'emresource_by_region.csv'))
@@ -80,16 +117,16 @@ def compare_ems( ems,channel):
 
 def plot_covidregions(channel,subgroups, psuffix) :
 
-    fig = plt.figure(figsize=(16, 8))
+    fig = plt.figure(figsize=(10, 5))
     fig.subplots_adjust(right=0.97, wspace=0.5, left=0.1, hspace=0.9, top=0.95, bottom=0.07)
     palette = sns.color_palette('Set1', len(exp_names))
-    axes = [fig.add_subplot(3, 4, x + 1) for x in range(len(subgroups))]
+    #axes = [fig.add_subplot(3, 4, x + 1) for x in range(len(subgroups))]
+    axes = [fig.add_subplot(1, 1, x + 1) for x in range(len(subgroups))]
 
     for c, region_suffix in enumerate(subgroups) :
 
         region_label= region_suffix.replace('_EMS-', 'covid region ')
         ems = int(region_suffix.replace('_EMS-', ''))
-
 
         for d, exp_name in enumerate(exp_names) :
             sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
@@ -104,13 +141,39 @@ def plot_covidregions(channel,subgroups, psuffix) :
     plt.savefig(os.path.join(sim_output_path, 'covidregion_'+psuffix+'_%s.png' % channel))
     plt.savefig(os.path.join(sim_output_path, 'covidregion'+psuffix+'_%s.pdf' % channel))
 
+def plot_covidregions_both(subgroups, plot_name) :
+
+    fig = plt.figure(figsize=(10, 4))
+    fig.subplots_adjust(right=0.97, wspace=0.5, left=0.1, hspace=0.9, top=0.95, bottom=0.07)
+    #axes = [fig.add_subplot(3, 4, x + 1) for x in range(len(subgroups))]
+    axes = [fig.add_subplot(1, 2, x + 1) for x in range(2)]
+
+    region_suffix =subgroups[0]
+    region_label= region_suffix.replace('_EMS-', 'covid region ')
+    ems_nr = int(region_suffix.replace('_EMS-', ''))
+
+    for d, exp_name in enumerate(exp_names) :
+        sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
+        df = load_sim_data(exp_name, region_suffix=region_suffix)
+        exp_name_label =  int(exp_name.split('_')[0])
+        plot_on_fig2(df, axes, ems_nr=ems_nr,  label=exp_name_label)
+
+    #axes[-1].legend()
+    #fig.suptitle(f'{region_label}\n', x=0.5, y=0.990)
+    #plt.tight_layout(rect=[0, 0, 0, .95])
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(sim_output_path, plot_name))
+    plt.savefig(os.path.join(sim_output_path, plot_name))
+
 
 if __name__ == '__main__' :
 
-    exp_names = ['20200910_IL_test_v3']
+    exp_names = ['20201020_IL_mr_baseline']
 
     covidregionlist = ['_EMS-1', '_EMS-2', '_EMS-3', '_EMS-4', '_EMS-5', '_EMS-6', '_EMS-7', '_EMS-8', '_EMS-9', '_EMS-10', '_EMS-11']
     #covidregionlist = ['_EMS-1', '_EMS-3', '_EMS-4', '_EMS-5', '_EMS-9']
 
-    plot_covidregions(channel='crit_det', subgroups = covidregionlist, psuffix ='_AugOct')
-    plot_covidregions(channel='hosp_det', subgroups = covidregionlist,  psuffix ='_AugOct')
+    #plot_covidregions(channel='crit_det', subgroups = covidregionlist, psuffix ='_reg11_JulDec')
+    #plot_covidregions(channel='hosp_det', subgroups = covidregionlist,  psuffix ='_reg11_JulDec')
+    plot_covidregions_both(subgroups = ['_EMS-11'],  plot_name ='covidregion_11_ICU_nonICU_JulDec')
