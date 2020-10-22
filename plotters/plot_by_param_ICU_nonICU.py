87d20f124f27119f1ddc0ab3cbba5923864d10f5
ManuelaRunge <manuela.runge@northwestern.edu>
2020-10-22 07:57:27 -0500

    edited plot_by_param_ICU_nonICU plots, included whole distribution

--- plotters/plot_by_param_ICU_nonICU.py
+++ plotters/plot_by_param_ICU_nonICU.py
@@ -15,7 +15,7 @@ mpl.rcParams['pdf.fonttype'] = 42
 datapath, projectpath, wdir,exe_dir, git_dir = load_box_paths()
 
 first_day = date(2020, 2, 13) # IL
-first_plot_day = date(2020, 7, 1)
+first_plot_day = date(2020, 8, 1)
 last_plot_day = date(2020,12, 30)
 
 
@@ -38,17 +38,18 @@ def plot_on_fig(df, c, axes,channel, color,panel_heading, ems, label=None, addgr
     ax = axes[c]
     df['date'] = df['time'].apply(lambda x: first_day + timedelta(days=int(x)))
     df = df[(df['date'] >= first_plot_day) & (df['date'] <= last_plot_day)]
-    mdf = df.groupby('date')[channel].agg([CI_50, CI_2pt5, CI_97pt5, CI_25, CI_75]).reset_index()
+    mdf = df.groupby('date')[channel].agg([np.min,CI_50, CI_2pt5, CI_97pt5, CI_25, CI_75,np.max]).reset_index()
 
     if addgrid:
         ax.grid(b=True, which='major', color='#999999', linestyle='-', alpha=0.3)
     ax.plot(mdf['date'], mdf['CI_50'], color=color, label=label)
-    ax.fill_between(mdf['date'].values, mdf['CI_25'], mdf['CI_75'],
-                color=color, linewidth=0, alpha=0.4)
+    ax.fill_between(mdf['date'].values, mdf['CI_25'], mdf['CI_75'],color=color, linewidth=0, alpha=0.4)
+    ax.fill_between(mdf['date'].values, mdf['CI_2pt5'], mdf['CI_97pt5'],color=color, linewidth=0, alpha=0.4)
+    ax.fill_between(mdf['date'].values, mdf['amin'], mdf['amax'],color=color, linewidth=0, alpha=0.1)
     ax.set_title(panel_heading, y=0.85)
-    formatter = mdates.DateFormatter("%d\n%b")
-    ax.xaxis.set_major_formatter(formatter)
-    ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+    #formatter = mdates.DateFormatter("%d\n%b")
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d\n%b'))
+    #ax.xaxis.set_major_locator(mdates.WeekdayLocator())
 
     ref_df  = compare_ems(ems=ems, channel=channel)
 
@@ -63,8 +64,8 @@ def plot_on_fig(df, c, axes,channel, color,panel_heading, ems, label=None, addgr
     ax.plot(ref_df['date'], ref_df[datachannel].rolling(window=7, center=True).mean(), c='k', alpha=1.0)
 
     capacity = load_capacity(ems)
-    ax.plot([np.min(mdf['date']), np.max(mdf['date'])],
-                [capacity[capacitychannel], capacity[capacitychannel]], '--', linewidth=1, color='black')
+    ax.plot([np.min(mdf['date']), np.max(mdf['date'])],[capacity[capacitychannel], capacity[capacitychannel]], '--', linewidth=1, color='black')
+    ax.plot([np.min(mdf['date']), np.max(mdf['date'])],[capacity[capacitychannel]*0.75, capacity[capacitychannel]*0.75], '--', linewidth=0.8, color='grey')
 
 def plot_on_fig2(df, axes,  ems_nr, label=None, addgrid=True) :
     palette = sns.color_palette('Set1', 2)
@@ -117,11 +118,11 @@ def compare_ems( ems,channel):
 
 def plot_covidregions(channel,subgroups, psuffix) :
 
-    fig = plt.figure(figsize=(10, 5))
+    fig = plt.figure(figsize=(14, 12))
     fig.subplots_adjust(right=0.97, wspace=0.5, left=0.1, hspace=0.9, top=0.95, bottom=0.07)
     palette = sns.color_palette('Set1', len(exp_names))
     #axes = [fig.add_subplot(3, 4, x + 1) for x in range(len(subgroups))]
-    axes = [fig.add_subplot(1, 1, x + 1) for x in range(len(subgroups))]
+    axes = [fig.add_subplot(4, 3, x + 1) for x in range(len(subgroups))]
 
     for c, region_suffix in enumerate(subgroups) :
 
@@ -132,7 +133,7 @@ def plot_covidregions(channel,subgroups, psuffix) :
             sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
             df = load_sim_data(exp_name, region_suffix=region_suffix)
             exp_name_label =  int(exp_name.split('_')[0])
-            plot_on_fig(df, c, axes, channel=channel, color=palette[d],ems=ems, panel_heading = region_label, label=exp_name_label)
+            plot_on_fig(df, c, axes, channel=channel, color=palette[d],ems=ems, panel_heading = region_label, label="")
 
         axes[-1].legend()
         #fig.suptitle(x=0.5, y=0.999,t=channel)
@@ -173,7 +174,7 @@ if __name__ == '__main__' :
     covidregionlist = ['_EMS-1', '_EMS-2', '_EMS-3', '_EMS-4', '_EMS-5', '_EMS-6', '_EMS-7', '_EMS-8', '_EMS-9', '_EMS-10', '_EMS-11']
     #covidregionlist = ['_EMS-1', '_EMS-3', '_EMS-4', '_EMS-5', '_EMS-9']
 
-    #plot_covidregions(channel='crit_det', subgroups = covidregionlist, psuffix ='_reg11_JulDec')
-    #plot_covidregions(channel='hosp_det', subgroups = covidregionlist,  psuffix ='_reg11_JulDec')
-    plot_covidregions_both(subgroups = ['_EMS-10'],  plot_name ='covidregion_10_ICU_nonICU_JulDec')
-    plt.show()
+    plot_covidregions(channel='crit_det', subgroups = covidregionlist, psuffix ='AugDec')
+    plot_covidregions(channel='hosp_det', subgroups = covidregionlist,  psuffix ='AugDec')
+    #plot_covidregions_both(subgroups = ['_EMS-10'],  plot_name ='covidregion_10_ICU_nonICU_JulDec')
+    #plt.show()
