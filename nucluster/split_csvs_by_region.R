e286874b598fcf9255e891ea85479af908e43129
ManuelaRunge <manuela.runge@northwestern.edu>
2020-10-26 04:52:48 -0500

    add script combining trajectories and saving subsets per region

---
+++ nucluster/split_csvs_by_region.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(data.table)
+
+exp_name <- "20201025_IL_mr_local_20201003_IL_mr_fitkistartsm3_sm5and6"
+exp_dir <- file.path("/projects/p30781/covidproject/covid-chicago/_temp/",exp_name)
+ 
+f_save_per_grp <- function(filepattern, i,SAVE="csv"){
+
+    fittingParam <- c('ki_multiplier_6' , 'ki_multiplier_7', 'ki_multiplier_time_6', 'ki_multiplier_time_7') 
+    outcomeParam <- paste0(c("death_det_cumul", "crit_det", "hosp_det", "hosp_det_cumul", "infected_cumul"), paste0("_EMS-", i))
+    KeepCols <- c("time", "startdate", "scen_num", "sample_num", fittingParam, outcomeParam)
+    
+    trajectoriesFiles <- list.files(exp_dir, pattern=filepattern)
+    
+    subdatList <- list()
+    for(tfile in trajectoriesFiles){
+      subdatList[[length(subdatList)+1]] <- fread(file.path(exp_dir,tfile), select = KeepCols)
+    }
+  
+    subdat <- subdatList %>% bind_rows()
+   
+  if(tolower(SAVE)=="csv")fwrite(subdat, file.path(exp_dir, paste0("trajectoriesDat_region_",i,".csv")))
+  if(tolower(SAVE)=="rdata")save(subdat,file=file.path(exp_dir, paste0("trajectoriesDat_region_",i,".RData")))
+}
+
+
+for(i in c(1:11)){
+  f_save_per_grp( filepattern="trim.csv" ,i,SAVE="RData")
+}
