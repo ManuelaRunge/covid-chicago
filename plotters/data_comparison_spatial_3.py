dacf77bbdf6d3e7a39d7b23240f2de93e3f72003
ManuelaRunge <manuela.runge@northwestern.edu>
2020-11-12 02:32:30 -0600

    edited data_comparison_spatial_3

--- plotters/data_comparison_spatial_3.py
+++ plotters/data_comparison_spatial_3.py
@@ -18,7 +18,7 @@ from processing_helpers import *
 
 mpl.rcParams['pdf.fonttype'] = 42
 today = datetime.today()
-datetoday = date(today.year, today.month, today.day)
+datetoday = date(2021,1,1) #date(today.year, today.month, today.day)
 
 
 def parse_args():
@@ -50,11 +50,11 @@ def parse_args():
 def load_sim_data(exp_name, ems_nr, input_wdir=None, fname='trajectoriesDat.csv', input_sim_output_path=None,
                   column_list=None):
     input_wdir = input_wdir or wdir
-    sim_output_path_base = os.path.join(input_wdir, 'simulation_output', exp_name)
-    sim_output_path =  sim_output_path_base
+    sim_output_path_base = os.path.join(input_wdir, 'simulation_output','_overflow_simulations', exp_name)
+    sim_output_path = sim_output_path_base
 
     df = pd.read_csv(os.path.join(sim_output_path, fname), usecols=column_list)
-
+    df['run_num']=-9
     # df.columns = df.columns.str.replace('_All', '')
     df.columns = df.columns.str.replace('_EMS-' + str(ems_nr), '')
     #df['infected_cumul'] = df['infected'] + df['recovered'] + df['deaths']
@@ -72,21 +72,21 @@ def load_sim_data(exp_name, ems_nr, input_wdir=None, fname='trajectoriesDat.csv'
 
 def plot_sim_and_ref(df, ems_nr, ref_df, channels, data_channel_names, titles, first_day=date(2020, 2, 22),
                      ymax=40, logscale=True):
-    fig = plt.figure(figsize=(15, 8))
+    fig = plt.figure(figsize=(17, 8))
     palette = sns.color_palette('husl', 12)
     k = 0
     for c, channel in enumerate(channels):
         ax = fig.add_subplot(2, 3, c + 1)
 
-        # for k, (ki, kdf) in enumerate(df.groupby('Ki')) :
-        mdf = df.groupby(['time','recovery_time_crit'])[channel].agg([np.mean, CI_2pt5, CI_97pt5, CI_25, CI_75]).reset_index()
+        mdf = df.groupby(['time','reopening_multiplier_4'])[channel].agg([np.mean, CI_2pt5, CI_97pt5, CI_25, CI_75]).reset_index()
 
-        for i, rtc in enumerate(mdf['recovery_time_crit'].unique()):
-            mdf_sub = mdf[mdf['recovery_time_crit'] == rtc]
+        for i, rtc in enumerate(mdf['reopening_multiplier_4'].unique()):
+            mdf_sub = mdf[mdf['reopening_multiplier_4'] == rtc]
             dates = [first_day + timedelta(days=int(x)) for x in mdf_sub['time']]
             ax.plot(dates, mdf_sub['mean'], color=palette[i], label=rtc)
 
-        ax.fill_between(dates, mdf_sub['CI_2pt5'], mdf_sub['CI_97pt5'], color=palette[i], linewidth=0, alpha=0.2)
+        mdf_all = df.groupby(['time'])[channel].agg([np.mean, CI_2pt5, CI_97pt5, CI_25, CI_75]).reset_index()
+        ax.fill_between(dates, mdf_all['CI_2pt5'], mdf_all['CI_97pt5'], color=palette[k], linewidth=0, alpha=0.2)
 
         ax.set_title(titles[c], y=0.8, fontsize=12)
         # ax.legend()
@@ -112,33 +112,34 @@ def plot_sim_and_ref(df, ems_nr, ref_df, channels, data_channel_names, titles, f
     plot_name = 'compare_to_data_covidregion_' + str(ems_nr)
     if logscale == False:
         plot_name = plot_name + "_nolog"
+
+    if not os.path.exists(plot_path):
+        os.makedirs(plot_path)
+    if not os.path.exists(os.path.join(plot_path,'pdf')):
+        os.makedirs(os.path.join(plot_path,'pdf'))
     plt.savefig(os.path.join(plot_path, plot_name + '.png'))
     plt.savefig(os.path.join(plot_path, 'pdf', plot_name + '.pdf'), format='PDF')
     # return a
 
 
 def compare_ems(exp_name, fname, ems_nr=0):
-    column_list = ['time', 'startdate', 'scen_num', 'sample_num', 'run_num']
+    column_list = ['time', 'startdate', 'scen_num', 'sample_num']
 
-    outcome_channels = ['susceptible', 'infected', 'recovered', 'infected_cumul', 'asymp_cumul', 'asymp_det_cumul',
-                        'symp_mild_cumul', 'symp_severe_cumul', 'symp_mild_det_cumul',
-                        'symp_severe_det_cumul', 'hosp_det_cumul', 'hosp_cumul', 'detected_cumul', 'crit_cumul',
-                        'crit_det_cumul', 'death_det_cumul',
-                        'deaths', 'crit_det', 'critical', 'hosp_det', 'hospitalized']
+    outcome_channels = [ 'hosp_det_cumul', 'hosp_cumul',  'crit_cumul',
+                        'crit_det_cumul', 'death_det_cumul','deaths', 'crit_det',
+                         'critical', 'hosp_det', 'hospitalized']
 
     for channel in outcome_channels:
         column_list.append(channel + "_EMS-" + str(ems_nr))
 
     df = load_sim_data(exp_name, ems_nr, fname=fname, column_list=column_list)
     first_day = datetime.strptime(df['startdate'].unique()[0], '%Y-%m-%d')
-
     df['critical_with_suspected'] = df['critical']
-
     df['date'] = df['time'].apply(lambda x: first_day + timedelta(days=int(x)))
-    df = df[df['date'] <= datetime.today()]
+    df = df[df['date'].dt.date <= datetoday]
 
-    param="recovery_time_crit"
-    sampled_df = pd.read_csv(os.path.join(wdir, 'simulation_output', exp_name, "sampled_parameters.csv"), usecols=['scen_num', param])
+    param="reopening_multiplier_4"
+    sampled_df = pd.read_csv(os.path.join(exp_dir, "sampled_parameters.csv"), usecols=['scen_num', param])
     df = pd.merge(how='left', left=df, left_on='scen_num', right=sampled_df, right_on='scen_num')
 
     ref_df = load_ref_df(ems_nr)
@@ -161,16 +162,18 @@ def compare_ems(exp_name, fname, ems_nr=0):
 
 if __name__ == '__main__':
 
-    args = parse_args()
-    trajectoriesName = args.trajectoriesName
-    Location = args.Location
+    #args = parse_args()
+    trajectoriesName = 'trajectoriesDat.csv' #args.trajectoriesName
+    Location = 'Local' #args.Location
     datapath, projectpath, wdir, exe_dir, git_dir = load_box_paths(Location=Location)
 
-    stem = args.stem
-    exp_names = [x for x in os.listdir(os.path.join(wdir, 'simulation_output')) if stem in x]
+    #stem = args.stem
+    #exp_names = [x for x in os.listdir(os.path.join(wdir, 'simulation_output')) if stem in x]
+    exp_names = ['20201110_IL_baseline_gradual_reopening']
 
     for exp_name in exp_names:
-        plot_path = os.path.join(wdir, 'simulation_output', exp_name, '_plots')
+        exp_dir = os.path.join(wdir, 'simulation_output','_overflow_simulations', exp_name)
+        plot_path = os.path.join(exp_dir, '_plots')
         for ems_nr in range(1, 12):
             print("Start processing region " + str(ems_nr))
             compare_ems(exp_name, fname=trajectoriesName, ems_nr=int(ems_nr))
