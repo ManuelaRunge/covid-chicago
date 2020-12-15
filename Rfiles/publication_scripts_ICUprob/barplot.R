2c40dff67fc791e1c93dd7fbdf7ac6752e384949
ManuelaRunge <manuela.runge@northwestern.edu>
2020-12-15 11:13:09 -0600

    updated barplot

--- Rfiles/publication_scripts_ICUprob/barplot.R
+++ Rfiles/publication_scripts_ICUprob/barplot.R
@@ -8,6 +8,7 @@ source("processing_helpers.R")
 source("publication_scripts_ICUprob/functions.R")
 
 TwoCols_seq <- c("#00a79d", "#f7941d")
+capacity_col <- "#2a3b90"
 
 ## -------------------------------
 ## Run script
@@ -21,10 +22,11 @@ exp_names <- exp_names[grep("IL_regreopen",exp_names)]
 exp_names <- exp_names[c(grep("daysdelay",exp_names),grep("counterfactual",exp_names))]
 exp_names <- exp_names[!(grepl("_reopen",exp_names))]
 
-dat <- f_combineDat(sim_dir,exp_names, "peak_exceed_df.csv")
+dat <- f_combineDat(sim_dir,exp_names, "peak_exceed_df.csv") %>%  
+             filter(ems %in% c("EMS-1", "EMS-4", "EMS-11"))
 
+dat$region <- factor(dat$ems, levels = c(paste0("EMS-", c(1:11))), labels = paste0('Region ',c(1:11)))
 dat$scen_name <- gsub(paste0(simdate,"_IL_regreopen"), "", dat$exp_name)
-dat$scen_name <- gsub("20201210_IL_regreopen", "", dat$scen_name)
 dat <- dat %>% separate(scen_name, into = c("reopen", "delay", "rollback"), sep = "_")
 table(dat$rollback, dat$exp_name)
 dat$rollback[is.na(dat$rollback)] <- "counterfactual"
@@ -42,92 +44,51 @@ dat$capacity_multiplier_fct <- factor(dat$capacity_multiplier_fct,
   levels = c(fct_labels,"counterfactual"),
   labels = c(fct_labels,"counterfactual")
 )
+dat$capacity_multiplier_fct2 <- factor(dat$capacity_multiplier_fct,
+                                      levels = c(fct_labels,"counterfactual"),
+                                      labels = c(fct_labels,"counter\nfactual")
+)
 table(dat$capacity_multiplier_fct, exclude = NULL)
 dat$capacity_multiplier_fct
 
-#dat$perc_ICU_occup_50CI_lower = (dat$critical_50CI_lower/  dat$avg_resource_available)*100
-#dat$perc_ICU_occup_50CI_upper = (dat$critical_50CI_upper/  dat$avg_resource_available)*100
+dat$perc_ICU_occup_50CI_lower = (dat$critical_50CI_lower/  dat$avg_resource_available)*100
+dat$perc_ICU_occup_50CI_upper = (dat$critical_50CI_upper/  dat$avg_resource_available)*100
 dat$perc_ICU_occup_95CI_lower = (dat$critical_95CI_lower/  dat$avg_resource_available)*100
 dat$perc_ICU_occup_95CI_upper = (dat$critical_95CI_upper/  dat$avg_resource_available)*100
 dat$perc_ICU_occup_median = (dat$critical_median/  dat$avg_resource_available)*100
 
 
-dat$region <- factor(dat$ems, levels = c(paste0("EMS-", c(1:11)), "All"), labels = c(c(1:11), "All"))
+plotdat <- dat %>% filter((delay == "counterfactual" |delay == delay_val & rollback == rollback_val)) 
 
-
-palldat_best <- dat %>%
-  filter(ems %in% c("EMS-1", "EMS-4", "EMS-11") &
-    (delay == "counterfactual" |
-      delay == delay_val & rollback == rollback_val))
-
-palldat_worst <- dat %>%
-  filter(ems %in% c("EMS-1", "EMS-4", "EMS-11") &
-           (delay == "counterfactual" |
-              delay == delay_val[length(delay_val)] & rollback ==  rollback_val[length(rollback_val)]))
-
-p1dat <- dat %>%
-  filter(ems %in% c("EMS-1", "EMS-4", "EMS-11") &
-           (delay != "counterfactual" &
-              delay ==  delay_val &
-              rollback ==  rollback_val)) %>%
-  as.data.frame()
-
-
-p1bdat <- dat %>%
-  filter(ems %in% c("EMS-1", "EMS-4", "EMS-11") &
-           (delay != "counterfactual" &
-              delay ==  delay_val &
-              rollback ==  rollback_val)) %>%
-  as.data.frame()
-
-p2dat <- subset(dat, ems %in% c("EMS-1", "EMS-4", "EMS-11") & (delay == "counterfactual")) %>% as.data.frame()
-
-
-
-
-p1 <- ggplot(data = p1dat) +
-  geom_bar(aes(x = capacity_multiplier_fct, y = perc_ICU_occup_median, group = interaction(delay, rollback, reopen), fill = reopen),
-    stat = "identity", position = position_dodge(width = 0.91), width = 0.7
+customTheme <- f_getCustomTheme()
+pplot <- ggplot(data = plotdat) +
+  geom_bar(aes(x = capacity_multiplier_fct2, y = critical_median, group = reopen, fill = reopen),
+    stat = "identity", position = position_dodge(width = 0.91), width = 0.7, alpha=0.4
+ ) +
+  geom_bar(data=subset(dat,(delay == "counterfactual" |delay == delay_val & rollback == rollback_values[4])),
+           aes(x = capacity_multiplier_fct2, y = critical_median, group = reopen, fill = reopen),
+           stat = "identity", position = position_dodge(width = 0.91), width = 0.7
   ) +
-  geom_bar(data = p1bdat,aes(x = capacity_multiplier_fct, y = perc_ICU_occup_median, group = interaction(delay, rollback, reopen), fill = reopen),
-           stat = "identity", position = position_dodge(width = 0.91), width = 0.7, alpha=0.4
+  
+  geom_errorbar(aes(x = capacity_multiplier_fct2, ymin =critical_95CI_lower, ymax = critical_95CI_upper, group=reopen),
+    position = position_dodge(width = 0.91), width = 0
   ) +
-  geom_errorbar(aes(x = capacity_multiplier_fct, ymin = perc_ICU_occup_95CI_lower, ymax = perc_ICU_occup_95CI_upper, 
-                    group = interaction(delay, rollback, reopen)),
-    position = position_dodge(width = 0.91), width = 0.1
-  ) +
-  labs(y="% of ICU capacity at predicted peak\nuntil end of December", x="Trigger treshold\n(as % of ICU capacity at which mitigation is triggered )")+
-  geom_hline(aes(yintercept = 100), linetype="dashed", col="dodgerblue") +
-  facet_wrap(~region, scales = "free", ncol = 1) +
+  labs(y="Predictd peak in ICU census\nuntil end of December", 
+       x="Trigger threshold\n (% of ICU capacity)")+
+  geom_hline(aes(yintercept =avg_resource_available), linetype="dashed", col=capacity_col,size=1) +
+  geom_text(aes(x=1.5, y=avg_resource_available+(avg_resource_available*0.5), label="ICU capacity"), col=capacity_col)+
+  facet_wrap(~region, scales = "free", ncol = 1, strip.position="right") +
   scale_fill_manual(values = c(TwoCols_seq)) +
   geom_hline(yintercept = c(0))+
-  customTheme +
-  theme_minimal()
+  theme(legend.position="none",
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank())+
+  customTheme
 
+pplot
 
-p2 <- ggplot(data = p2dat) +
-  geom_bar(aes(x = capacity_multiplier_fct, y = perc_ICU_occup_median, group = interaction(delay, rollback, reopen), fill = reopen),
-    stat = "identity", position = position_dodge(width = 0.91), width = 0.7
-  ) +
-  geom_errorbar(aes(x = capacity_multiplier_fct, ymin = perc_ICU_occup_95CI_lower, ymax = perc_ICU_occup_95CI_upper, 
-                    group = interaction(delay, rollback, reopen)),
-    position = position_dodge(width = 0.91), width = 0.1
-  ) +
-  labs(y="% of ICU capacity at predicted peak\nuntil end of December", x="Trigger treshold\n(as % of ICU capacity at which mitigation is triggered )")+
-  geom_hline(aes(yintercept = 100), linetype="dashed", col="dodgerblue") +
-  facet_wrap(~region, scales = "free", ncol = 1) +
-  scale_fill_manual(values = c(TwoCols_seq)) +
-  customTheme +
-  geom_hline(yintercept = c(0))+
-  theme_minimal()
-
-pall <- plot_grid(p1+theme(legend.position = "none"),p2+labs(y=""), ncol=2, rel_widths = c(1,0.5))
-pall
-
-ggsave(paste0("barplot_pall.pdf"),
-  plot = pall,
-  path = file.path(sim_dir, "ICU_bar_plots"), width = 10, height = 8, device = "pdf"
-)
+f_save_plot(plot_name=paste0("barplot_pall"), pplot = pplot, 
+            plot_dir = file.path(sim_dir, "ICU_bar_plots"), width = 6, height = 8)
 
 dat_peak <-dat
 ###### Text to describe figures
